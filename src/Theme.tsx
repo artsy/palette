0b91aec95531ed76c0fd8b1e4885b9b209d45e36
Christopher Pappas <damassi.pappas@gmail.com>
2018-06-07 12:24:21 -0700

    Update README.md

--- src/Theme.tsx
+++ src/Theme.tsx
@@ -1,7 +1,23 @@
 import React from "react"
-import { ThemeProvider as ThemeP } from "styled-components"
+import reset from "styled-reset"
+import { GridThemeProvider, injectLayoutBaseCSS } from "styled-bootstrap-grid"
+import { ThemeProvider } from "styled-components"
 
-export const Theme = {
+// Notion: https://www.notion.so/artsy/Design-92030f16ed7c4c72bb3eb832b4243d04
+// API: https://jxnblk.com/styled-system/api
+// Table: https://jxnblk.com/styled-system/table
+
+export const themeProps = {
+  borders: [0, "2px solid"],
+
+  // buttonStyle util
+  buttons: {
+    primary: {
+      color: "purple100",
+      backgroundColor: "black",
+    },
+  },
+
   // https://www.notion.so/artsy/Color-a0c24896daf8433d9409aee2146ac267
   colors: {
     text: "#000",
@@ -21,9 +37,6 @@ export const Theme = {
     white100: "#FFF",
   },
 
-  // https://www.notion.so/artsy/Spacing-93eeaed9fdf9480099fec7094fd1b9f3
-  space: [0, 3, 5, 10, 20, 30, 40, 60, 90, 120, 180],
-
   fontFamily: {
     unica: {
       regular: "Unica77LL-Regular",
@@ -41,6 +54,54 @@ export const Theme = {
     },
   },
 
+  // https://github.com/dragma/Elements/Grid#props-definition
+  // https://www.notion.so/artsy/Grid-e489a52e26bd4319b6ee7898044a8a53
+  grid: {
+    breakpoints: {
+      giant: 1200,
+      xl: 1200,
+      desktop: 992,
+      lg: 992,
+      tablet: 768,
+      md: 768,
+      phone: 576,
+      sm: 576,
+      smaller: 575,
+      xs: 575,
+    },
+    container: {
+      padding: 40,
+    },
+    row: {
+      padding: 15,
+    },
+    col: {
+      padding: 15,
+    },
+  },
+
+  // https://www.notion.so/artsy/Spacing-93eeaed9fdf9480099fec7094fd1b9f3
+  space: [
+    0, // 0
+    3, // 1
+    5, // 2
+    10, // 3
+    20, // 4
+    30, // 5
+    40, // 6
+    60, // 7
+    90, // 8
+    120, // 9
+    180, // 10
+  ],
+
+  textStyles: {
+    caps: {
+      textTransform: "uppercase",
+    },
+  },
+
+  // https://www.notion.so/artsy/Typography-d1f9f6731f3d47c78003d6d016c30221
   typeSizes: {
     sans1: {
       fontSize: 10,
@@ -54,6 +115,52 @@ export const Theme = {
       fontSize: 14,
       lineHeight: 24,
     },
+    sans3t: {
+      fontSize: 14,
+      lineHeight: 20,
+    },
+    sans4: {
+      fontSize: 16,
+      lineHeight: 26,
+    },
+    sans4t: {
+      fontSize: 16,
+      lineHeight: 22,
+    },
+    sans5: {
+      fontSize: 18,
+      lineHeight: 30,
+    },
+    sans5t: {
+      fontSize: 18,
+      lineHeight: 26,
+    },
+    sans6: {
+      fontSize: 22,
+      lineHeight: 30,
+    },
+    sans8: {
+      fontSize: 28,
+      lineHeight: 36,
+    },
+    sans10: {
+      fontSize: 42,
+      lineHeight: 50,
+    },
+    sans12: {
+      fontSize: 60,
+      lineHeight: 66,
+    },
+    sans14: {
+      fontSize: 80,
+      lineHeight: 84,
+    },
+    sans16: {
+      fontSize: 100,
+      lineHeight: 104,
+    },
+
+    // Serif
     serif1: {
       fontSize: 12,
       lineHeight: 16,
@@ -62,6 +169,48 @@ export const Theme = {
       fontSize: 14,
       lineHeight: 18,
     },
+    serif3: {
+      fontSize: 16,
+      lineHeight: 24,
+    },
+    serif3t: {
+      fontSize: 16,
+      lineHeight: 20,
+    },
+    serif4: {
+      fontSize: 18,
+      lineHeight: 26,
+    },
+    serif4t: {
+      fontSize: 18,
+      lineHeight: 22,
+    },
+    serif5: {
+      fontSize: 22,
+      lineHeight: 32,
+    },
+    serif5t: {
+      fontSize: 22,
+      lineHeight: 28,
+    },
+    serif6: {
+      fontSize: 26,
+      lineHeight: 32,
+    },
+    serif8: {
+      fontSize: 32,
+      lineHeight: 38,
+    },
+    serif10: {
+      fontSize: 42,
+      lineHeight: 50,
+    },
+    serif12: {
+      fontSize: 60,
+      lineHeight: 66,
+    },
+
+    // Display
     display2: {
       fontSize: 10,
       lineHeight: 12,
@@ -83,10 +232,31 @@ export const Theme = {
       lineHeight: 22,
     },
   },
-
-  borders: [0, "1px #E5E5E5 solid"],
 }
 
-export const ThemeProvider = props => (
-  <ThemeP theme={Theme}>{props.children}</ThemeP>
-)
+export function injectGlobalCSS() {
+  injectLayoutBaseCSS(`
+    ${reset};
+
+    html,
+    body,
+    #root {
+      height: 100%;
+    }
+    body {
+      margin: 0;
+      padding: 0;
+    }
+  `)
+}
+
+export const Theme = props => {
+  return (
+    <ThemeProvider theme={themeProps}>
+      {/* TODO: Update for native */}
+      <GridThemeProvider gridTheme={themeProps.grid}>
+        {props.children}
+      </GridThemeProvider>
+    </ThemeProvider>
+  )
+}
