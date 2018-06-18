da4dffdcdd32be52341a4f1f56dc85a68f42f67e
Eloy Durán <eloy.de.enige@gmail.com>
2018-06-18 12:06:15 +0200

    [Typography] Make type-check green.

--- src/elements/Typography.tsx
+++ src/elements/Typography.tsx
@@ -1,4 +1,4 @@
-import React, { SFC } from "react"
+import React from "react"
 import styled from "styled-components"
 import { styled as primitives } from "../platform/primitives"
 import { themeProps } from "../Theme"
