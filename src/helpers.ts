172f359dae5d83681c2aa4e9b03fb3eafff591f0
Zephraph <zephraph@gmail.com>
2018-07-09 23:32:48 -0400

    [FEATURE types] Exposes type defs for colors and spacing values

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,13 +1,11 @@
-import { themeProps } from "./Theme"
+import { themeProps, Color, SpacingUnit } from "./Theme"
 
 /**
  * A helper to easily access colors when not in a styled-components or styled-systems context
  */
-export const color = (colorKey: keyof typeof themeProps["colors"]) =>
-  themeProps.colors[colorKey]
+export const color = (colorKey: Color) => themeProps.colors[colorKey]
 
 /**
  * A helper to easily access space values when not in a styled-components or styled-systems context
  */
-export const space = (spaceKey: keyof typeof themeProps["space"]) =>
-  themeProps.space[spaceKey]
+export const space = (spaceKey: SpacingUnit) => themeProps.space[spaceKey]
