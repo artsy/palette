8514cfafaa75fa7de8ea2581b2d6728ddf1581a8
Zephraph <zephraph@gmail.com>
2018-08-14 23:30:49 -0400

    Update docz

--- doczrc.js
+++ doczrc.js
@@ -2,7 +2,7 @@ module.exports = {
   typescript: true,
   protocol: "http",
   wrapper: "docs/components/Wrapper.tsx",
-  indexHtml: "docs/index.html",
+  // indexHtml: "docs/index.html",
   themeConfig: {
     // Can override any of these colors... https://github.com/pedronauck/docz/blob/781c2bff5ae6ebe7a0d8a46f9cbc8cfef709c191/packages/docz-theme-default/src/styles/modes.ts#L5
     colors: {
