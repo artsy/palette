af2990b77961f45e88c07e2636e958e6bbbbd807
Zephraph <zephraph@gmail.com>
2018-08-29 16:58:40 -0400

    Add nvmrc, move impl stuff to _utils, update docz

--- doczrc.js
+++ doczrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   typescript: true,
   protocol: "http",
-  wrapper: "docs/components/Wrapper.tsx",
+  wrapper: "docs/_utils/components/Wrapper.tsx",
   indexHtml: "docs/index.html",
   themeConfig: {
     // Can override any of these colors... https://github.com/pedronauck/docz/blob/781c2bff5ae6ebe7a0d8a46f9cbc8cfef709c191/packages/docz-theme-default/src/styles/modes.ts#L5
