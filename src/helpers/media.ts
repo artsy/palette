28e6727891f6c5d246a5572d0e5b99aeafe0e268
Christopher Pappas <damassi.pappas@gmail.com>
2018-09-06 16:25:11 -0700

    [PATCH helper] Update responsive helper

---
+++ src/helpers/media.ts
@@ -0,0 +1,32 @@
+import { breakpoints } from "../Theme"
+import { css } from "styled-components"
+
+type Media = { [S in keyof typeof breakpoints]: typeof css }
+
+/**
+ * A helper to construct media query strings for responsive style targeting.
+ *
+ * @example
+ *
+ * const Panel = styled.div`
+ *   ${media.sm`
+ *     width: 50%;
+ *   `}
+ *   ${media.lg`
+ *     width: 100%;
+ *   `}
+ * `
+ */
+export const media: Media = Object.entries(breakpoints).reduce(
+  (accumulator, [label, value]) => {
+    return {
+      ...accumulator,
+      [label]: (strings, ...args) => css`
+        @media (max-width: ${value}px) {
+          ${css(strings, ...args)};
+        }
+      `,
+    }
+  },
+  {}
+) as Media
---
+++ src/helpers/space.ts
@@ -0,0 +1,7 @@
+import { themeProps, SpacingUnit } from "../Theme"
+
+/**
+ * A helper to easily access space values when not in a styled-components or
+ * styled-systems context.
+ */
+export const space = (spaceKey: SpacingUnit) => themeProps.space[spaceKey]
