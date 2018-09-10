a0238d6ff3c2ba5ee5bfca61b7fbb2a3d779b1c3
Zephraph <zephraph@gmail.com>
2018-09-10 17:20:36 -0400

    Marginally less gross

--- docs/_theme/Sidebar.tsx
+++ docs/_theme/Sidebar.tsx
@@ -1,16 +1,25 @@
 import { Docs, Link } from "docz"
 import React from "react"
+import styled from "styled-components"
+import { color, Flex, Serif } from "../../src"
+
+const BorderedContainer = styled(Flex)`
+  border-right: 1px solid ${color("black10")};
+`
 
 export const Sidebar = () => (
-  <Docs>
-    {({ docs }) => (
-      <ul>
-        {docs.map(doc => (
-          <li key={doc.id}>
-            <Link to={doc.route}>{doc.name}</Link>
-          </li>
-        ))}
-      </ul>
-    )}
-  </Docs>
+  <BorderedContainer flexDirection="column" p={2} height="100%">
+    <Serif size="5">Palette</Serif>
+    <Docs>
+      {({ docs }) => (
+        <ul>
+          {docs.map(doc => (
+            <li key={doc.id}>
+              <Link to={doc.route}>{doc.name}</Link>
+            </li>
+          ))}
+        </ul>
+      )}
+    </Docs>
+  </BorderedContainer>
 )
--- docs/_theme/index.tsx
+++ docs/_theme/index.tsx
@@ -1,32 +1,34 @@
 import { DocPreview, theme } from "docz"
 import React from "react"
 
-import { Sans, Serif, Spinner, Theme as PaletteTheme } from "../../src"
+import { Box, Flex, Serif, Spinner, Theme as PaletteTheme } from "../../src"
 import { Sidebar } from "./Sidebar"
 
 const Theme = () => (
   <PaletteTheme>
-    <>
+    <Flex>
       <Sidebar />
-      <DocPreview
-        components={{
-          // page: components.Page,
-          // notFound: components.NotFound,
-          // render: components.Render,
-          h1: props => <Serif size="12" {...props} />,
-          h2: props => <Serif size="10" {...props} />,
-          h3: props => <Serif size="8" {...props} />,
-          h4: props => <Serif size="6" {...props} />,
-          // h5: components.H5,
-          // h6: components.H6,
-          // ul: components.List,
-          loading: props => <Spinner {...props} />,
-          // table: components.Table,
-          // pre: components.Pre,
-          // inlineCode: components.Code,
-        }}
-      />
-    </>
+      <Box p={2}>
+        <DocPreview
+          components={{
+            // page: components.Page,
+            // notFound: components.NotFound,
+            // render: components.Render,
+            h1: props => <Serif size="12" {...props} />,
+            h2: props => <Serif size="10" {...props} />,
+            h3: props => <Serif size="8" {...props} />,
+            h4: props => <Serif size="6" {...props} />,
+            // h5: components.H5,
+            // h6: components.H6,
+            // ul: components.List,
+            loading: props => <Spinner {...props} />,
+            // table: components.Table,
+            // pre: components.Pre,
+            // inlineCode: components.Code,
+          }}
+        />
+      </Box>
+    </Flex>
   </PaletteTheme>
 )
 
