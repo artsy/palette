ad9c855ad94c7b1b07ce77a0c23784d2675e54a9
Jon Allured <jon.allured@gmail.com>
2018-10-11 16:36:42 -0500

    Add basic dismissable banner

--- src/elements/Banner/Banner.tsx
+++ src/elements/Banner/Banner.tsx
@@ -1,10 +1,28 @@
 import React from "react"
+import styled from "styled-components"
 
 import { space } from "../../helpers"
 import { Box } from "../Box"
 import { Sans } from "../Typography"
 
+const Target = styled.div`
+  height: 30px;
+  position: absolute;
+  right: 11px;
+  top: 11px;
+  width: 30px;
+
+  &:hover {
+    cursor: pointer;
+  }
+`
+
+const CloseButton = ({ onClick }) => {
+  return <Target onClick={onClick}>X</Target>
+}
+
 export interface BannerProps {
+  dismissable: boolean
   message: string
 }
 
@@ -12,10 +30,32 @@ export interface BannerProps {
  * A banner
  */
 export class Banner extends React.Component<BannerProps> {
+  static defaultProps = {
+    dismissable: false,
+  }
+
+  state = {
+    dismissed: false,
+  }
+
+  handleCloseClick = () => {
+    this.setState({ dismissed: true })
+  }
+
   render() {
+    if (this.state.dismissed) return null
+    const showCloseButton = this.props.dismissable
+
     return (
-      <Box bg="red100" color="white100" p={space(2)} textAlign="center">
+      <Box
+        bg="red100"
+        color="white100"
+        p={space(2)}
+        position="relative"
+        textAlign="center"
+      >
         <Sans size="2">{this.props.message}</Sans>
+        {showCloseButton && <CloseButton onClick={this.handleCloseClick} />}
       </Box>
     )
   }
