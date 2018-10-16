b1f0b6388d1257249e3e5eb90ac39d8e8de88eb8
Jon Allured <jon.allured@gmail.com>
2018-10-16 08:26:58 -0500

    Fix broken attr name

--- src/svgs/CloseIcon/CloseIcon.tsx
+++ src/svgs/CloseIcon/CloseIcon.tsx
@@ -13,7 +13,7 @@ export const CloseIcon: React.SFC<IconProps> = ({ fill }) => (
   <svg width="12" height="12" xmlns="http://www.w3.org/2000/svg">
     <path
       fill={color(fill)}
-      fill-rule="nonzero"
+      fillRule="nonzero"
       d="M5.03 5.904L.189.986 1.159 0 6 4.919 10.841 0l.97.986L6.97 5.904l5.03 5.11-.97.986L6 6.89.97 12 0 11.014l5.03-5.11z"
     />
   </svg>
