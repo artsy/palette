656ea6851316bd1a61ec4ab975ca3fb1951a9ded
oxaudo <oksana@artsymail.com>
2018-11-26 11:11:29 -0500

    add placeholder for ios link file

--- src/elements/Link/Link.tsx
+++ src/elements/Link/Link.tsx
@@ -1,14 +1,11 @@
 import styled from "styled-components"
-import {
-  color as styledColor,
-  ColorProps,
-  space,
-  SpaceProps,
-} from "styled-system"
+import { color as styledColor, space, SpaceProps } from "styled-system"
 import { color } from "../../helpers"
+import { Color } from "../../Theme"
 
-export interface LinkProps extends SpaceProps, ColorProps {
+export interface LinkProps extends SpaceProps {
   noUnderline?: boolean
+  color?: Color
 }
 
 /**
@@ -17,17 +14,16 @@ export interface LinkProps extends SpaceProps, ColorProps {
 export const Link = styled.a<LinkProps>`
   ${space};
   ${styledColor};
-  text-decoration: ${props =>
-    props.noUnderline || props.color ? "none" : "underline"};
   cursor: pointer;
   transition: color 0.25s;
-  :hover {
+  text-decoration: ${props =>
+    props.noUnderline || props.color ? "none" : "underline"};
+  &:hover {
     text-decoration: ${props => (props.color ? "none" : "underline")};
     color: ${color("black100")};
   }
-  :focus {
+  &:focus {
     border: 1px solid ${color("purple100")};
-    color: ${props =>
-      props.color ? color(props.color as any) : color("black100")};
+    color: ${props => (props.color ? color(props.color) : color("black100"))};
   }
 `
