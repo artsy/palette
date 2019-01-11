b008f468bdb5951b59e505397d14c83fb21a7dc9
Christopher Pappas <damassi.pappas@gmail.com>
2019-01-10 22:29:52 -0800

    [Docs v2] Add Gatsby

---
+++ docs2/gatsby-config.js
@@ -0,0 +1,10 @@
+const path = require("path")
+
+module.exports = {
+  siteMetadata: {
+    title: "Palette",
+    description: "Artsy's design system",
+    author: "Artsy",
+  },
+  plugins: ["gatsby-plugin-styled-components", "gatsby-plugin-typescript"],
+}
---
+++ docs2/src/components/Layout.tsx
@@ -0,0 +1,27 @@
+import { graphql, StaticQuery } from "gatsby"
+import React from "react"
+
+import { Header } from "components/Header"
+
+export const Layout = ({ children }) => (
+  <StaticQuery
+    query={graphql`
+      query SiteTitleQuery {
+        site {
+          siteMetadata {
+            title
+          }
+        }
+      }
+    `}
+    render={data => (
+      <>
+        <Header siteTitle={data.site.siteMetadata.title} />
+        <div>
+          {children}
+          <footer>Add footer</footer>
+        </div>
+      </>
+    )}
+  />
+)
