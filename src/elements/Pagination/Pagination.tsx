e83393f2860d11dff16347b56b96106849b46dfc
Adam Iskounen <adam.iskounen@artsymail.com>
2019-01-17 17:01:14 -0500

    Fixed an incorrect import line.

--- src/elements/Pagination/Pagination.tsx
+++ src/elements/Pagination/Pagination.tsx
@@ -1,7 +1,7 @@
 import React from "react"
 import styled, { css } from "styled-components"
 
-import { Box, Flex, Sans, Separator } from ".."
+import { Box, Flex, Sans, Separator } from "../"
 import { color, space } from "../../helpers"
 import { ChevronIcon } from "../../svgs"
 
