46fd9e6386c4d895351146b557a702e1b1942fc6
Christopher Pappas <damassi.pappas@gmail.com>
2019-01-17 16:46:58 -0800

    Fix pagination arrow alignment

--- src/svgs/ChevronIcon.tsx
+++ src/svgs/ChevronIcon.tsx
@@ -1,4 +1,6 @@
 import React from "react"
+import styled from "styled-components"
+import { space, SpaceProps, top, TopProps } from "styled-system"
 import { color } from "../helpers"
 
 type Direction = "left" | "right" | "up" | "down"
@@ -11,12 +13,18 @@ export enum Rotation {
   DOWN = "rotate(270deg)",
 }
 
-interface IconProps {
+interface IconProps extends SpaceProps, TopProps {
   direction?: Direction | Rotation
   fill?: string
   size?: number
   style?: React.CSSProperties
 }
+
+const Icon = styled.svg<IconProps>`
+  position: relative;
+  ${space};
+  ${top};
+`
 
 /** Icon */
 export const ChevronIcon = ({
@@ -24,6 +32,7 @@ export const ChevronIcon = ({
   fill = color("black100"),
   size = 20,
   style = {},
+  ...props
 }: IconProps) => {
   const Left = <path d="M27.3 34.7L17.6 25l9.7-9.7 1.4 1.4-8.3 8.3 8.3 8.3z" />
   const Right = <path d="M22.7 34.7l-1.4-1.4 8.3-8.3-8.3-8.3 1.4-1.4 9.7 9.7z" /> // prettier-ignore
@@ -31,13 +40,14 @@ export const ChevronIcon = ({
   const Down = <path d="M25 32.4l-9.7-9.7 1.4-1.4 8.3 8.3 8.3-8.3 1.4 1.4z" />
 
   return (
-    <svg
+    <Icon
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 50 50"
       fill={fill}
       width={size}
       height={size}
       style={style}
+      {...props}
     >
       {(() => {
         switch (direction) {
@@ -61,6 +71,6 @@ export const ChevronIcon = ({
             return Right
         }
       })()}
-    </svg>
+    </Icon>
   )
 }
