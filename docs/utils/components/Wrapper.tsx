960a5d5b121e1e8dab3dcde78073514b71e9dfce
Christopher Pappas <damassi.pappas@gmail.com>
2019-02-03 18:53:54 -0800

    [DELETE] Remove old docs

--- docs/utils/components/Wrapper.tsx
+++
@@ -1,6 +0,0 @@
-import React from "react"
-import { Theme as PaletteTheme } from "../../../src/Theme"
-
-export default function Theme({ children }) {
-  return <PaletteTheme>{children}</PaletteTheme>
-}
