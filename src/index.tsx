c0faf8ee44ae850ec4ddeb4bcc0c568409d7df06
Christopher Pappas <damassi.pappas@gmail.com>
2019-02-03 21:07:28 -0800

    Update icon page

--- src/index.tsx
+++ src/index.tsx
@@ -2,3 +2,7 @@ export * from "./elements"
 export * from "./helpers"
 export * from "./svgs"
 export * from "./Theme"
+
+// Helpers
+import * as _AllIcons from "./svgs"
+export const AllIcons = _AllIcons
