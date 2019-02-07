00a505abc5f1b07102a9500ddf1eca54e47ced60
Christopher Pappas <damassi.pappas@gmail.com>
2019-02-06 19:59:05 -0800

    Collaps nav on url click

--- www/src/Boot.tsx
+++ www/src/Boot.tsx
@@ -1,17 +1,25 @@
 import { Theme } from "@artsy/palette"
 import { MDXProvider } from "@mdx-js/tag"
+import { Location } from "@reach/router"
+import { NavState } from "components/Sidebar/NavState"
 import React from "react"
 import { Provider as StateProvider } from "unstated"
 import { GlobalStyles, MarkdownComponents } from "./components/GlobalComponents"
 
 export const Boot = ({ element }) => {
   return (
-    <StateProvider>
-      <MDXProvider components={MarkdownComponents}>
-        <Theme>
-          <GlobalStyles>{element}</GlobalStyles>
-        </Theme>
-      </MDXProvider>
-    </StateProvider>
+    <Location>
+      {({ location }) => {
+        return (
+          <StateProvider inject={[new NavState(location.pathname)]}>
+            <MDXProvider components={MarkdownComponents}>
+              <Theme>
+                <GlobalStyles>{element}</GlobalStyles>
+              </Theme>
+            </MDXProvider>
+          </StateProvider>
+        )
+      }}
+    </Location>
   )
 }
