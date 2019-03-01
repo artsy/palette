0be15c40b035a026617add3e06ee9081ca8ef90b
Christopher Pappas <damassi.pappas@gmail.com>
2019-02-28 20:00:22 -0800

    Add icons

--- src/svgs/SoloIcon.tsx
+++ src/svgs/SoloIcon.tsx
@@ -1,17 +1,17 @@
 import React from "react"
 import { Icon, IconProps } from "./Icon"
 
-/** Icon */
+/** Solo Icon */
 export const SoloIcon: React.SFC<IconProps> = props => {
   return (
-    <Icon width="25" height="25" xmlns="http://www.w3.org/2000/svg" {...props}>
-      <g fill="none" fillRule="evenodd">
-        <path d="M0 0h25v25H0z" />
-        <g transform="translate(7.294 4.941)" stroke="#000">
-          <path d="M0 13.717V10.86a3 3 0 0 1 3-3h4.195a3 3 0 0 1 3 3v2.856" />
-          <circle cx="5.053" cy="2.807" r="2.807" />
-        </g>
-      </g>
+    <Icon
+      xmlns="http://www.w3.org/2000/svg"
+      width="18"
+      height="18"
+      viewBox="0 0 18 18"
+      {...props}
+    >
+      <path d="M9 3a2.51 2.51 0 1 1 0 5.02A2.51 2.51 0 0 1 9 3zm0-1a3.51 3.51 0 1 0 0 7.02A3.51 3.51 0 0 0 9 2zm6 11v3h-1v-3a1.5 1.5 0 0 0-1.5-1.5h-7A1.5 1.5 0 0 0 4 13v3H3v-3a2.5 2.5 0 0 1 2.5-2.5h7A2.5 2.5 0 0 1 15 13z" />
     </Icon>
   )
 }
