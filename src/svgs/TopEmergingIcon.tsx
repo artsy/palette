006bed095ba0db0a2488168f995ad3562a8a6f69
Jon Allured <jon.allured@gmail.com>
2019-03-11 15:39:26 -0500

    Prune a little more noise

--- src/svgs/TopEmergingIcon.tsx
+++ src/svgs/TopEmergingIcon.tsx
@@ -7,7 +7,7 @@ export const TopEmergingIcon: React.SFC<IconProps> = props => {
   return (
     <Icon {...props} viewBox="0 0 18 18">
       <title>top emerging</title>
-      <g fill="none" fillRule="evenodd">
+      <g>
         <path
           d="M9.036 16.071A7.036 7.036 0 1 0 9.036 2a7.036 7.036 0 0 0 0 14.071zm0 1A8.036 8.036 0 1 1 9.036 1a8.036 8.036 0 0 1 0 16.071z"
           fill={color(props.fill)}
