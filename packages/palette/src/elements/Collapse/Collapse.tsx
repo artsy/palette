87e5e127d68f918588de3781054c2dfb81ce0970
David Sheldrick <djsheldrick@gmail.com>
2019-03-25 13:38:41 +0100

    custom web collapse component

--- packages/palette/src/elements/Collapse/Collapse.tsx
+++ packages/palette/src/elements/Collapse/Collapse.tsx
@@ -1,35 +1,89 @@
 import React from "react"
-import { animated, Spring } from "react-spring"
 
-export interface CollapseProps {
-  /** Determines whether content is expanded or collapsed */
-  open: boolean
-}
+/**
+ * Collapse component for the web
+ */
+export class Collapse extends React.Component<{ open: boolean }> {
+  wrapperModifyTimeout: ReturnType<typeof setTimeout>
+  wrapperRef: HTMLDivElement | null = null
 
-/** Collapses content with animation when open is not true */
-export class Collapse extends React.Component<CollapseProps> {
-  state = {
-    mounted: false,
+  onTransitionEnd = (ev: TransitionEvent) => {
+    if (!this.wrapperRef) {
+      return
+    }
+    if (ev.propertyName === "height") {
+      this.wrapperRef.style.height = this.props.open ? "auto" : "0px"
+    }
+  }
+
+  firstRender: { open: boolean } | null = {
+    open: this.props.open,
   }
 
   componentDidMount() {
-    this.setState({ mounted: true })
+    if (!this.wrapperRef) {
+      return
+    }
+
+    this.wrapperRef.addEventListener("transitionend", this.onTransitionEnd)
+  }
+
+  componentDidUpdate() {
+    if (!this.wrapperRef) {
+      return
+    }
+    if (this.props.open && this.wrapperRef.style.height !== "auto") {
+      // open
+      const prevHeight = this.wrapperRef.style.height || "0px"
+      this.wrapperRef.style.height = "auto"
+      const actualHeight = this.wrapperRef.offsetHeight
+      this.wrapperRef.style.height = prevHeight
+      this.wrapperModifyTimeout = setTimeout(() => {
+        this.wrapperRef.style.height = actualHeight + "px"
+      }, 10)
+    } else if (!this.props.open && this.wrapperRef.style.height !== "0px") {
+      // close
+      const actualHeight = this.wrapperRef.offsetHeight
+      this.wrapperRef.style.height = actualHeight + "px"
+      this.wrapperModifyTimeout = setTimeout(() => {
+        this.wrapperRef.style.height = 0 + "px"
+      }, 10)
+    }
+  }
+
+  componentWillUnmount() {
+    this.wrapperRef.removeEventListener("transitionend", this.onTransitionEnd)
+    clearTimeout(this.wrapperModifyTimeout)
   }
 
   render() {
+    const { children, open } = this.props
+    // render explicit height before first change, to let us render closed
+    // elements on the server
+    let heightProps = {}
+    if (this.firstRender) {
+      // render() might be called multiple times before the first time `open` changes
+      if (this.firstRender.open !== open) {
+        // `open` prop has changed for the first time
+        // ditch explicit height and let `componentDidUpdate` take the wheel
+        this.firstRender = null
+      } else {
+        heightProps = {
+          height: open ? "auto" : "0px",
+        }
+      }
+    }
     return (
-      <Spring
-        native
-        immediate={!this.state.mounted}
-        from={{ height: 0 }}
-        to={{ height: this.props.open ? "auto" : 0 }}
+      <div
+        ref={ref => (this.wrapperRef = ref)}
+        style={{
+          transition: "height 0.3s ease",
+          overflow: "hidden",
+          ...heightProps,
+        }}
       >
-        {props => (
-          <animated.div style={{ ...props, overflow: "hidden" }}>
-            {this.props.children}
-          </animated.div>
-        )}
-      </Spring>
+        {children}
+      </div>
     )
   }
 }
