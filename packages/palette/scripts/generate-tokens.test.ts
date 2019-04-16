e0a87412a99a05e0c0a88d8cf008797b6c28637f
Zephraph <zephraph@gmail.com>
2019-04-15 22:18:50 -0400

    Remove token test

--- packages/palette/scripts/generate-tokens.test.ts
+++
@@ -1,9 +0,0 @@
-import { themeProps } from "../src/Theme"
-
-import tokens from "../tokens.json"
-
-// if this test fails, get them back in sync like this:
-// $ yarn generate-tokens
-it("tokens file does not drift from the theme props", () => {
-  expect(tokens).toEqual(themeProps)
-})
