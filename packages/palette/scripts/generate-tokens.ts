67645112013e582d9a75f1cc2a1e704c634dc75c
Zephraph <zephraph@gmail.com>
2019-04-15 22:15:11 -0400

    Generate a stylesheet for non-react systems

--- packages/palette/scripts/generate-tokens.ts
+++ packages/palette/scripts/generate-tokens.ts
@@ -3,6 +3,6 @@ import { themeProps } from "../src/Theme"
 import * as fs from "fs"
 
 const data = JSON.stringify(themeProps, null, 2)
-const path = "tokens.json"
+const path = "dist/tokens.json"
 
 fs.writeFileSync(path, data, "utf8")
