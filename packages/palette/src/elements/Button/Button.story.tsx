7ecae30349fa74bfce800a798069bc0fe873901a
Zephraph <zephraph@gmail.com>
2019-04-26 16:17:30 -0400

    Add some visual separation between buttons

--- packages/palette/src/elements/Button/Button.story.tsx
+++ packages/palette/src/elements/Button/Button.story.tsx
@@ -6,16 +6,16 @@ storiesOf("Components/Button", module)
   .add("small", () => {
     return (
       <>
-        <Button size="small" variant="primaryBlack">
+        <Button size="small" variant="primaryBlack" mr={1}>
           primaryBlack
         </Button>
-        <Button size="small" variant="primaryWhite">
+        <Button size="small" variant="primaryWhite" mr={1}>
           primaryWhite
         </Button>
-        <Button size="small" variant="secondaryGray">
+        <Button size="small" variant="secondaryGray" mr={1}>
           secondaryGray
         </Button>
-        <Button size="small" variant="secondaryOutline">
+        <Button size="small" variant="secondaryOutline" mr={1}>
           secondaryOutline
         </Button>
         <Button size="small" variant="noOutline">
@@ -27,16 +27,16 @@ storiesOf("Components/Button", module)
   .add("medium", () => {
     return (
       <>
-        <Button size="medium" variant="primaryBlack">
+        <Button size="medium" variant="primaryBlack" mr={1}>
           primaryBlack
         </Button>
-        <Button size="medium" variant="primaryWhite">
+        <Button size="medium" variant="primaryWhite" mr={1}>
           primaryWhite
         </Button>
-        <Button size="medium" variant="secondaryGray">
+        <Button size="medium" variant="secondaryGray" mr={1}>
           secondaryGray
         </Button>
-        <Button size="medium" variant="secondaryOutline">
+        <Button size="medium" variant="secondaryOutline" mr={1}>
           secondaryOutline
         </Button>
         <Button size="medium" variant="noOutline">
@@ -48,16 +48,16 @@ storiesOf("Components/Button", module)
   .add("large", () => {
     return (
       <>
-        <Button size="large" variant="primaryBlack">
+        <Button size="large" variant="primaryBlack" mr={1}>
           primaryBlack
         </Button>
-        <Button size="large" variant="primaryWhite">
+        <Button size="large" variant="primaryWhite" mr={1}>
           primaryWhite
         </Button>
-        <Button size="large" variant="secondaryGray">
+        <Button size="large" variant="secondaryGray" mr={1}>
           secondaryGray
         </Button>
-        <Button size="large" variant="secondaryOutline">
+        <Button size="large" variant="secondaryOutline" mr={1}>
           secondaryOutline
         </Button>
         <Button size="large" variant="noOutline">
