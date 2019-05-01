3c40f172a1d9e40477d9817c30cfaf66597be56a
Luc Succes <l2succes@gmail.com>
2019-05-01 17:02:44 -0400

    Add new properties for button

--- packages/palette-framer.framerfx/code/Button.tsx
+++ packages/palette-framer.framerfx/code/Button.tsx
@@ -1,14 +1,30 @@
 import * as React from "react"
 import { PropertyControls, ControlType } from "framer"
-import { Button as BaseButton, Theme } from "@artsy/palette"
+import {
+  Button as BaseButton,
+  ButtonVariant,
+  ButtonSize,
+  Theme,
+} from "@artsy/palette"
 
-type Props = { text: string }
+interface Props {
+  text: string
+  type: ButtonVariant
+  size: ButtonSize
+  width?: number
+  fullWidth: boolean
+}
 
 export class Button extends React.Component<Props> {
   render() {
+    const { fullWidth, size, text, type, width } = this.props
+    const extraProps = fullWidth ? { width: width } : {}
+
     return (
       <Theme>
-        <BaseButton>{this.props.text}</BaseButton>
+        <BaseButton variant={type} size={size} {...extraProps}>
+          {text}
+        </BaseButton>
       </Theme>
     )
   }
@@ -17,23 +33,43 @@ export class Button extends React.Component<Props> {
   // https://reactjs.org/docs/react-component.html#defaultprops
   static defaultProps: Props = {
     text: "Hello World!",
+    type: "primaryBlack",
+    size: "medium",
+    fullWidth: false,
   }
 
   // Add Framer UI for this component (in the properties panel)
   // https://framer.com/learn/docs/components#code
   static propertyControls: PropertyControls<Props> = {
     text: { type: ControlType.String, title: "Text" },
+    type: {
+      type: ControlType.Enum,
+      title: "Type",
+      options: [
+        "primaryBlack",
+        "primaryWhite",
+        "secondaryGray",
+        "secondaryOutline",
+        "noOutline",
+      ],
+      optionTitles: [
+        "Primary Black",
+        "Primary White",
+        "Secondary Gray",
+        "Secondary Outline",
+        "No Outline",
+      ],
+    },
+    size: {
+      type: ControlType.Enum,
+      title: "Size",
+      options: ["small", "medium", "large"],
+      optionTitles: ["Small", "Medium", "Large"],
+    },
+    fullWidth: {
+      type: ControlType.Boolean,
+      disabledTitle: "Fixed",
+      enabledTitle: "Dynamic",
+    },
   }
-}
-
-// Define some standard CSS for your component
-const style: React.CSSProperties = {
-  height: "100%",
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "center",
-  textAlign: "center",
-  color: "#8855FF",
-  background: "rgba(136, 85, 255, 0.1)",
-  overflow: "hidden",
 }
