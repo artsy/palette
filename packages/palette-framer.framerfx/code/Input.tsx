a60265318568648a1b78085821d94d04d29cc905
Luc Succes <l2succes@gmail.com>
2019-05-02 13:48:22 -0400

    [Framer] Adds more options to Input component

--- packages/palette-framer.framerfx/code/Input.tsx
+++ packages/palette-framer.framerfx/code/Input.tsx
@@ -2,18 +2,35 @@ import * as React from "react"
 import { PropertyControls, ControlType } from "framer"
 import { Input as PaletteInput } from "@artsy/palette"
 
-type Props = { text: string }
+interface Props {
+  text: string
+  title?: string
+  placeholder: string
+  required?: boolean
+  disabled?: boolean
+}
 
 export class Input extends React.Component<Props> {
-  // Return the component contents in JSX
-  // https://reactjs.org/docs/introducing-jsx.html
   render() {
-    return <PaletteInput />
+    const { disabled, placeholder, text, title, required } = this.props
+    return (
+      <PaletteInput
+        placeholder={placeholder}
+        title={title}
+        disabled={disabled}
+        required={required}
+        value={text}
+      />
+    )
   }
 
   // Add Framer UI for this component (in the properties panel)
   // https://framer.com/learn/docs/components#code
   static propertyControls: PropertyControls<Props> = {
+    title: { type: ControlType.String, title: "Title" },
     text: { type: ControlType.String, title: "Text" },
+    placeholder: { type: ControlType.String, title: "Placeholder" },
+    required: { type: ControlType.Boolean },
+    disabled: { type: ControlType.Boolean },
   }
 }
