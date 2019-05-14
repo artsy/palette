522a747f809f22adce3a6275081445e9f5c97564
Adam Iskounen <adam.iskounen@artsymail.com>
2019-05-14 04:48:06 -0400

    Displays a Hello World line chart component.

---
+++ packages/palette/src/elements/LineChart/LineChart.tsx
@@ -0,0 +1,7 @@
+/**
+ * LineChart is a component that displays some data ponts connected by lines.
+ * Useful for visualizing a time series, etc.
+ */
+export const LineChart = () => {
+  return "Hello, world!"
+}
