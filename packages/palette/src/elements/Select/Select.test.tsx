2391a15f3979b2c3aca3d91089bfc6002374320d
oxaudo <oksana@artsymail.com>
2019-05-28 14:47:51 -0400

    rename MiniSelect to SelectSmall and create separate entry for it in docs

--- packages/palette/src/elements/Select/Select.test.tsx
+++ packages/palette/src/elements/Select/Select.test.tsx
@@ -1,9 +1,9 @@
 import { mount } from "enzyme"
 import React from "react"
-import { MiniSelect } from "../Select"
+import { SelectSmall } from "../Select"
 
 describe("Select", () => {
-  describe("MiniSelect", () => {
+  describe("SelectSmall", () => {
     const options = [
       {
         text: "First option",
@@ -16,7 +16,7 @@ describe("Select", () => {
     ]
     it("renders proper options with correct selected one", () => {
       const wrapper = mount(
-        <MiniSelect options={options} selected="secondOption" />
+        <SelectSmall options={options} selected="secondOption" />
       )
 
       expect(wrapper.find("option").length).toBe(2)
@@ -25,7 +25,7 @@ describe("Select", () => {
 
     it("triggers callback on change", () => {
       const spy = jest.fn()
-      const wrapper = mount(<MiniSelect options={options} onSelect={spy} />)
+      const wrapper = mount(<SelectSmall options={options} onSelect={spy} />)
       wrapper
         .find("option")
         .at(1)
@@ -34,7 +34,7 @@ describe("Select", () => {
     })
 
     it("supports title attribute and renders it properly", () => {
-      const wrapper = mount(<MiniSelect options={options} title="Sort" />)
+      const wrapper = mount(<SelectSmall options={options} title="Sort" />)
 
       expect(wrapper.html()).toContain("Sort:")
     })
