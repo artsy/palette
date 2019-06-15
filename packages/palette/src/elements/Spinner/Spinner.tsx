4f35c8552c5f48ac2f6d27223d77f3625da4f949
Luc Succes <l2succes@gmail.com>
2019-06-15 00:31:38 -0400

    Add React Native version of Spinner

--- packages/palette/src/elements/Spinner/Spinner.tsx
+++ packages/palette/src/elements/Spinner/Spinner.tsx
@@ -1,15 +1,8 @@
 // @ts-ignore
 import React from "react"
 import styled, { keyframes } from "styled-components"
-
-export interface SpinnerProps {
-  /** Width of the spinner */
-  width?: number
-  /** Height of the spinner */
-  height?: number
-  /** Size of the spinner */
-  size?: "small" | "medium" | "large"
-}
+import { color } from "../../helpers"
+import { getSize, SpinnerProps } from "./Spinner.shared"
 
 const spin = keyframes`
   100% {
@@ -17,36 +10,8 @@ const spin = keyframes`
   }
 `
 
-const getSize = (props: SpinnerProps) => {
-  const base = { width: 25, height: 6 }
-
-  switch (props.size) {
-    case "small":
-      return {
-        width: base.width * 0.5,
-        height: base.height * 0.5,
-      }
-    case "medium":
-      return {
-        width: base.width * 0.8,
-        height: base.height * 0.8,
-      }
-    case "large":
-      return {
-        width: base.width,
-        height: base.height,
-      }
-    default:
-      return {
-        width: props.width,
-        height: props.height,
-      }
-  }
-}
-
 /** Generic Spinner component */
 export const Spinner = styled.div<SpinnerProps>`
-  background: black;
   animation: ${spin} 1s infinite linear;
   position: absolute;
 
@@ -54,6 +19,7 @@ export const Spinner = styled.div<SpinnerProps>`
     const { width, height } = getSize(props)
 
     return `
+      background: ${color(props.color)};
       width: ${width}px;
       height: ${height}px;
       top: calc(50% - ${height}px / 2);
@@ -65,6 +31,7 @@ export const Spinner = styled.div<SpinnerProps>`
 Spinner.defaultProps = {
   width: 25,
   height: 6,
+  color: "black100",
 }
 
 Spinner.displayName = "Spinner"
