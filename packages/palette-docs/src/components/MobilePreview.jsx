5493d2900f0c9509be8d2c61f3bd169e7a7872b4
Luc Succes <l2succes@gmail.com>
2019-06-25 09:46:25 -0400

    Update MobilePreview component

--- packages/palette-docs/src/components/MobilePreview.jsx
+++ packages/palette-docs/src/components/MobilePreview.jsx
@@ -1,4 +1,20 @@
 import React from "react"
 import { IPhone } from "./iPhone"
+import styled from "styled-components"
+import { Container } from "unstated"
 
-export const MobilePreview = ({ children }) => <IPhone>{children}</IPhone>
+export const MobilePreview = ({ children }) => (
+  <IPhone>
+    <InnerContainer>{children}</InnerContainer>
+  </IPhone>
+)
+
+const InnerContainer = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  overflow-y: auto;
+  padding: 0 5px;
+`
