278bc1ba2052f08252ce2785521697fd91f9c556
David Sheldrick <d.j.sheldrick@gmail.com>
2019-07-12 17:22:50 +0100

    Add close icon ios version

---
+++ packages/palette/src/svgs/CloseIcon.ios.tsx
@@ -0,0 +1,17 @@
+import React from "react"
+import { Path } from "react-native-svg"
+import { color } from "../helpers"
+import { Icon, IconProps } from "./Icon"
+
+/** CloseIcon */
+export const CloseIcon: React.SFC<IconProps> = props => {
+  return (
+    <Icon {...props} viewBox="0 0 18 18">
+      <Path
+        d="M9.88 9l4.56 4.56-.88.88L9 9.88l-4.56 4.56-.88-.88L8.12 9 3.56 4.44l.88-.88L9 8.12l4.56-4.56.88.88z"
+        fill={color(props.fill)}
+        fillRule="evenodd"
+      />
+    </Icon>
+  )
+}
