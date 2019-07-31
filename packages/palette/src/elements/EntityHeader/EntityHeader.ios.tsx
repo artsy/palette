cbf8873552d94a84aab5192845aa78b8231bd03e
David Sheldrick <d.j.sheldrick@gmail.com>
2019-07-31 10:09:38 -0400

    remove explicit widths on entity header ios

--- packages/palette/src/elements/EntityHeader/EntityHeader.ios.tsx
+++ packages/palette/src/elements/EntityHeader/EntityHeader.ios.tsx
@@ -1,5 +1,4 @@
 import React, { SFC } from "react"
-import { Dimensions } from "react-native"
 import { Avatar } from "../Avatar"
 import { Flex } from "../Flex"
 import { SpacerProps } from "../Spacer"
@@ -15,14 +14,6 @@ interface EntityHeaderProps extends SpacerProps {
 }
 
 /**
- * FIXME: We're currently needing to hardcode these widths so that the
- * text will have a fixed width to allow for the trailing ellipses
- */
-const PADDING_WIDTH = 40
-const BUTTON_WIDTH = 80
-const IMAGE_WIDTH = 60
-
-/**
  * Component that is used as entity header that is paired with rich information about the entity.
  * Spec: zpl.io/aNoYM6d
  */
@@ -35,13 +26,11 @@ export const EntityHeader: SFC<EntityHeaderProps> = ({
   FollowButton,
   ...remainderProps
 }) => {
-  const viewWidth = Dimensions.get("window").width
   return (
     <Flex
       flexDirection="row"
       justifyContent="space-between"
       flexWrap="nowrap"
-      width={viewWidth - PADDING_WIDTH}
       {...remainderProps}
     >
       {(imageUrl || initials) && (
@@ -49,11 +38,7 @@ export const EntityHeader: SFC<EntityHeaderProps> = ({
           <Avatar size="xs" src={imageUrl} initials={initials} />
         </Flex>
       )}
-      <Flex
-        ml="2px"
-        justifyContent="center"
-        width={viewWidth - PADDING_WIDTH - BUTTON_WIDTH - IMAGE_WIDTH}
-      >
+      <Flex ml="2px" justifyContent="center" width={0} flexGrow={1}>
         <Serif
           ellipsizeMode="tail"
           numberOfLines={1}
@@ -79,7 +64,6 @@ export const EntityHeader: SFC<EntityHeaderProps> = ({
       {FollowButton && (
         <Flex
           ml={1}
-          width={BUTTON_WIDTH}
           flexDirection="row"
           alignItems="center"
           justifyContent="flex-end"
