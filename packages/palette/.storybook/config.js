48f88972cc3086ab942ddc5439c2acfbb5d2a507
Devon Blandin <dblandin@gmail.com>
2019-08-21 13:46:13 -0400

    Inject global styles into Storybook environment
    Apply global styles to the environment where we render Storybook
    examples.

--- packages/palette/.storybook/config.js
+++ packages/palette/.storybook/config.js
@@ -2,6 +2,8 @@ import React from "react"
 import { configure, addDecorator, addParameters } from "@storybook/react"
 import { create } from "@storybook/theming"
 import { Theme } from "../src/Theme"
+import { injectGlobalStyles } from "../src/helpers/injectGlobalStyles"
+
 import "storybook-chromatic"
 
 // automatically import all files ending in *.story.tsx.
@@ -27,6 +29,15 @@ addParameters({
   },
 })
 
-addDecorator(storyFn => <Theme>{storyFn()}</Theme>)
+const { GlobalStyles } = injectGlobalStyles()
+
+addDecorator(storyFn => (
+  <Theme>
+    <>
+      <GlobalStyles />
+      {storyFn()}
+    </>
+  </Theme>
+))
 
 configure(loadStories, module)
