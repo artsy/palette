5fb6c0f7aef81f2ee15af13b5f3e0f56e0f13896
oxaudo <oksana@artsymail.com>
2019-10-02 15:08:43 -0400

    filling out the spec

--- packages/palette/src/elements/Dialog/Dialog.story.tsx
+++ packages/palette/src/elements/Dialog/Dialog.story.tsx
@@ -3,54 +3,42 @@ import React from "react"
 import { Dialog } from "./Dialog"
 
 storiesOf("Components/Dialog", module)
-  .add(
-    "Minimal",
-    () => {
-      return (
-        <Dialog
-          title="Here is a dialog which is modal"
-          primaryCta={{
-            action: () => ({}),
-            text: "Continue",
-          }}
-        />
-      )
-    },
-    { chromatic: { delay: 500, diffThreshold: 0.2 } }
-  )
-  .add(
-    "With detail",
-    () => {
-      return (
-        <Dialog
-          title="Information"
-          detail="This extra informaton is important."
-          primaryCta={{
-            action: () => ({}),
-            text: "Continue",
-          }}
-        />
-      )
-    },
-    { chromatic: { delay: 500, diffThreshold: 0.2 } }
-  )
-  .add(
-    "With secondary Cta",
-    () => {
-      return (
-        <Dialog
-          title="Information"
-          detail="This extra informaton is important."
-          primaryCta={{
-            action: () => ({}),
-            text: "Continue",
-          }}
-          secondaryCta={{
-            action: () => ({}),
-            text: "Cancel",
-          }}
-        />
-      )
-    },
-    { chromatic: { delay: 500, diffThreshold: 0.2 } }
-  )
+  .add("Minimal", () => {
+    return (
+      <Dialog
+        title="Here is a dialog which is modal"
+        primaryCta={{
+          action: () => ({}),
+          text: "Continue",
+        }}
+      />
+    )
+  })
+  .add("With detail", () => {
+    return (
+      <Dialog
+        title="Information"
+        detail="This extra informaton is important."
+        primaryCta={{
+          action: () => ({}),
+          text: "Continue",
+        }}
+      />
+    )
+  })
+  .add("With secondary Cta", () => {
+    return (
+      <Dialog
+        title="Information"
+        detail="This extra informaton is important."
+        primaryCta={{
+          action: () => ({}),
+          text: "Continue",
+        }}
+        secondaryCta={{
+          action: () => ({}),
+          text: "Cancel",
+        }}
+      />
+    )
+  })
