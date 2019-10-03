d6c7b38011e56c4c8f7a212386cbd2f5481d4493
Jon Allured <jon.allured@gmail.com>
2019-10-03 16:38:34 -0500

    Improve test coverage for selects

--- packages/palette/src/elements/Select/Select.test.tsx
+++ packages/palette/src/elements/Select/Select.test.tsx
@@ -1,19 +1,58 @@
 import { mount } from "enzyme"
 import React from "react"
-import { SelectSmall } from "../Select"
+import { LargeSelect, SelectSmall } from "../Select"
+
+const options = [
+  {
+    text: "First option",
+    value: "firstOption",
+  },
+  {
+    text: "Second option that is really long",
+    value: "secondOption",
+  },
+]
 
 describe("Select", () => {
+  describe("LargeSelect", () => {
+    it("renders the options provided", () => {
+      const props = { options }
+      const wrapper = mount(<LargeSelect {...props} />)
+      expect(wrapper.find("option").length).toEqual(2)
+    })
+
+    it("renders some optional info", () => {
+      const props = {
+        description: "This is the description",
+        error: "This is the error",
+        options,
+        title: "This is the title",
+      }
+      const wrapper = mount(<LargeSelect {...props} />)
+      expect(wrapper.text()).toContain("This is the description")
+      expect(wrapper.text()).toContain("This is the error")
+      expect(wrapper.text()).toContain("This is the title")
+    })
+
+    it("can be marked required", () => {
+      const props = {
+        options,
+        required: true,
+        title: "This is the title",
+      }
+      const wrapper = mount(<LargeSelect {...props} />)
+      expect(wrapper.find("Required").length).toEqual(1)
+    })
+
+    it("passes the name attr down to the raw node", () => {
+      const name = "some_rails_thing"
+      const props = { name, options }
+      const wrapper = mount(<LargeSelect {...props} />)
+      expect(wrapper.find("select").prop("name")).toEqual(name)
+    })
+  })
+
   describe("SelectSmall", () => {
-    const options = [
-      {
-        text: "First option",
-        value: "firstOption",
-      },
-      {
-        text: "Second option that is really long",
-        value: "secondOption",
-      },
-    ]
     it("renders proper options with correct selected one", () => {
       const wrapper = mount(
         <SelectSmall options={options} selected="secondOption" />
