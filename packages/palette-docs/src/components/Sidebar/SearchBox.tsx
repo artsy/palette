3ca4a5817e841ad3bebe89b06f93aa1f83dc1fa4
Steven Hicks <steven.j.hicks@gmail.com>
2020-02-24 11:13:33 -0600

    Style algolia search results to look more like palette

--- packages/palette-docs/src/components/Sidebar/SearchBox.tsx
+++ packages/palette-docs/src/components/Sidebar/SearchBox.tsx
@@ -1,13 +1,20 @@
 import { Input } from "@artsy/palette"
 import React, { useEffect } from "react"
+import "./algolia.css"
+
+declare global {
+  interface Window {
+    docsearch: any
+    docsearchSettings: any
+  }
+}
 
 export function SearchBox() {
   useEffect(() => {
     if (window.docsearchSettings === undefined) {
       return
     }
-    console.log("doing the thing with ", window.docsearchSettings)
-    docsearch({
+    window.docsearch({
       apiKey: window.docsearchSettings.apiKey,
       indexName: window.docsearchSettings.indexName,
       inputSelector: "#search",
