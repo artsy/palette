d8bed5efb2298109474895e02c20ee6ea6f4d8e3
dzucconi <mail@damonzucconi.com>
2020-06-02 11:06:08 -0400

    Adds stories for Image

---
+++ packages/palette/src/elements/Image/Image.story.tsx
@@ -0,0 +1,25 @@
+import { storiesOf } from "@storybook/react"
+import React from "react"
+import { Image } from "../Image"
+
+storiesOf("Components/Image", module)
+  .add("Image", () => {
+    return (
+      <Image
+        width="300px"
+        height="200px"
+        src="https://picsum.photos/300/200/?random"
+      />
+    )
+  })
+  .add("LazyImage", () => {
+    return [...new Array(100)].map((_, i) => (
+      <Image
+        key={i}
+        lazyLoad
+        width="300px"
+        height="200px"
+        src={`https://picsum.photos/300/200/?random=${i}`}
+      />
+    ))
+  })
