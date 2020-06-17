53e6c6d56f1d92714b1f2552ccc9aad36cb108e1
dzucconi <mail@damonzucconi.com>
2020-06-17 10:30:59 -0400

    Configures viewport to use our breakpoints in addition to the defaults

--- packages/palette/.storybook/config.js
+++ packages/palette/.storybook/config.js
@@ -3,10 +3,11 @@ import { configure, addDecorator, addParameters } from "@storybook/react"
 import { create } from "@storybook/theming"
 import { Theme } from "../src/Theme"
 import { injectGlobalStyles } from "../src/helpers/injectGlobalStyles"
-
+import { INITIAL_VIEWPORTS } from "@storybook/addon-viewport"
+import { breakpoints } from "../src/Theme"
 import "storybook-chromatic"
 
-// automatically import all files ending in *.story.tsx.
+// Automatically import all files ending in *.story.tsx.
 const req = require.context("../src", true, /\.story\.tsx$/)
 function loadStories() {
   req
@@ -22,7 +23,6 @@ addParameters({
       brandTitle: "Palette",
       brandUrl: "https://palette.artsy.net",
     }),
-
     inline: true,
     showPanel: false,
     sortStoriesByKind: true,
@@ -39,5 +39,29 @@ addDecorator(storyFn => (
     </>
   </Theme>
 ))
+
+const viewports = Object.entries(breakpoints).reduce(
+  (memo, [key, width]) => ({
+    ...memo,
+    [key]: {
+      name: `breakpoint: ${key}`,
+      styles: {
+        width,
+        height: "100%",
+      },
+    },
+  }),
+  {}
+)
+
+addParameters({
+  viewport: {
+    viewports: {
+      ...viewports,
+      ...INITIAL_VIEWPORTS,
+    },
+    defaultViewport: "reset",
+  },
+})
 
 configure(loadStories, module)
