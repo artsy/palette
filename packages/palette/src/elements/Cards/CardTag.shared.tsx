91d3da878153b50f5f38160d0b63af9e59276eca
Pavlos Vinieratos <pvinis@gmail.com>
2020-06-29 14:32:50 +0200

    support palette Colors

--- packages/palette/src/elements/Cards/CardTag.shared.tsx
+++ packages/palette/src/elements/Cards/CardTag.shared.tsx
@@ -1,9 +1,10 @@
 import { StyleProp, ViewStyle } from "react-native"
+import { Color } from "../../Theme"
 
 export interface CardTagProps {
   text: string
-  textColor: string
-  color: string
-  borderColor?: string
+  textColor: Color
+  color: Color
+  borderColor?: Color
   style?: StyleProp<ViewStyle>
 }
