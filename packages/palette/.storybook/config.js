64e1f359535d38d628ec6ca273cfd28754e021f9
dzucconi <mail@damonzucconi.com>
2020-07-07 14:37:55 -0400

    Removes theme

--- packages/palette/.storybook/config.js
+++ packages/palette/.storybook/config.js
@@ -1,6 +1,5 @@
 import React from "react"
 import { configure, addDecorator, addParameters } from "@storybook/react"
-import { create } from "@storybook/theming"
 import { Theme } from "../src/Theme"
 import { injectGlobalStyles } from "../src/helpers/injectGlobalStyles"
 import { INITIAL_VIEWPORTS } from "@storybook/addon-viewport"
@@ -18,11 +17,6 @@ function loadStories() {
 
 addParameters({
   options: {
-    theme: create({
-      base: "light",
-      brandTitle: "Palette",
-      brandUrl: "https://palette.artsy.net",
-    }),
     inline: true,
     showPanel: false,
     sortStoriesByKind: true,
