a7053084ed61665fc8c6f9b8fbae22ee4fb97bc7
dzucconi <mail@damonzucconi.com>
2020-07-21 15:08:15 -0400

    Compose props and adds types

--- packages/palette/src/elements/Grid/Grid.tsx
+++ packages/palette/src/elements/Grid/Grid.tsx
@@ -4,28 +4,55 @@ import {
   Row as _Row,
 } from "styled-bootstrap-grid"
 import styled from "styled-components"
-import { color, flex, maxWidth, space, textAlign, width } from "styled-system"
+import {
+  border,
+  BorderProps,
+  color,
+  ColorProps,
+  compose,
+  flex,
+  FlexProps,
+  maxWidth,
+  MaxWidthProps,
+  space,
+  SpaceProps,
+  textAlign,
+  TextAlignProps,
+  width,
+  WidthProps,
+} from "styled-system"
 
 /** Outer wrapper when using a grid */
-export const Grid: any = styled(_Container)`
+export const Grid = styled(_Container)<
+  SpaceProps & MaxWidthProps & BorderProps
+>`
   max-width: ${props => props.theme.grid.breakpoints.xl}px;
-  ${space};
-  ${maxWidth};
+  ${compose(
+    space,
+    maxWidth,
+    border
+  )};
 `
 
 /** Grid row */
-export const Row: any = styled(_Row)`
-  ${color};
-  ${space};
+export const Row = styled(_Row)<ColorProps & SpaceProps>`
+  ${compose(
+    color,
+    space
+  )};
 `
 
 /** Grid column */
-export const Col: any = styled(_Col)`
-  ${color};
-  ${flex};
-  ${space};
-  ${textAlign};
-  ${width};
+export const Col = styled(_Col)<
+  ColorProps & FlexProps & SpaceProps & TextAlignProps & WidthProps
+>`
+  ${compose(
+    color,
+    flex,
+    space,
+    textAlign,
+    width
+  )}
 `
 
 Grid.displayName = "Grid"
