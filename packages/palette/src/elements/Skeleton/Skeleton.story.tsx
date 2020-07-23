cbdf6d00d038313285c21a2a6b3dfb9c4343620e
dzucconi <mail@damonzucconi.com>
2020-07-23 13:14:13 -0400

    Adds support for SkeletonText; specifies SkeletonBox

--- packages/palette/src/elements/Skeleton/Skeleton.story.tsx
+++ packages/palette/src/elements/Skeleton/Skeleton.story.tsx
@@ -1,13 +1,53 @@
 import { storiesOf } from "@storybook/react"
 import React from "react"
-import { Skeleton } from "./Skeleton"
+import { Box } from "../Box"
+import { SkeletonBox, SkeletonText } from "./Skeleton"
 
-storiesOf("Components/Skeleton", module).add("Basic", () => {
-  return (
-    <>
-      <Skeleton m={2} width={400} height={300} />
-      <Skeleton m={2} width={400} height={300} done />
-      <Skeleton m={2} width={400} height={300} borderRadius="1em" />
-    </>
-  )
-})
+storiesOf("Components/Skeleton", module)
+  .add("SkeletonBox", () => {
+    return (
+      <>
+        <SkeletonBox m={2} width={400} height={300} />
+        <SkeletonBox m={2} width={400} height={300} done />
+        <SkeletonBox m={2} width={400} height={300} borderRadius="1em" />
+      </>
+    )
+  })
+  .add("SkeletonText", () => {
+    return (
+      <>
+        <SkeletonText variant="text" m={1} borderRadius={4}>
+          loading
+        </SkeletonText>
+
+        <SkeletonText variant="mediumText" m={1} borderRadius={4}>
+          still waiting...
+        </SkeletonText>
+
+        <SkeletonText variant="title" m={1} borderRadius={4}>
+          please wait
+        </SkeletonText>
+
+        <SkeletonText variant="largeTitle" m={1} borderRadius={4}>
+          hold
+        </SkeletonText>
+      </>
+    )
+  })
+  .add("Example placeholder", () => {
+    return (
+      <Box display="flex" alignItems="center" px={2} py={1}>
+        <SkeletonBox width={40} height={40} mr={1} />
+
+        <Box>
+          <SkeletonText variant="small" borderRadius={2}>
+            Pending Artwork Title
+          </SkeletonText>
+
+          <SkeletonText variant="small" borderRadius={2}>
+            Pending Artist
+          </SkeletonText>
+        </Box>
+      </Box>
+    )
+  })
--- packages/palette/src/elements/Skeleton/Skeleton.tsx
+++ packages/palette/src/elements/Skeleton/Skeleton.tsx
@@ -1,6 +1,10 @@
+import React from "react"
 import styled, { css, keyframes } from "styled-components"
+import { border, BorderProps } from "styled-system"
 import { color } from "../../helpers"
+import { splitProps } from "../../utils/splitProps"
 import { Box, BoxProps } from "../Box"
+import { Text, TextProps } from "../Text"
 
 const PULSE = keyframes`
   0% { background-color: ${color("black10")}; }
@@ -8,13 +12,11 @@ const PULSE = keyframes`
   100% { background-color: ${color("black10")}; }
 `
 
-/** SkeletonProps */
-export type SkeletonProps = BoxProps & {
+interface DoneProps {
   done?: boolean
 }
 
-/** Skeleton */
-export const Skeleton = styled(Box)<{ done?: boolean }>`
+const Skeleton = styled(Box)<DoneProps>`
   ${({ done }) =>
     done
       ? css`
@@ -24,3 +26,51 @@ export const Skeleton = styled(Box)<{ done?: boolean }>`
           animation: ${PULSE} 2s ease-in-out infinite;
         `}
 `
+
+/** SkeletonProps */
+export type SkeletonBoxProps = BoxProps & DoneProps
+
+/** Skeleton */
+export const SkeletonBox: React.FC<SkeletonBoxProps> = ({ done, ...rest }) => {
+  return <Skeleton aria-busy={!done} done={done} borderRadius={2} {...rest} />
+}
+
+const splitBorderProps = splitProps<BorderProps>(border)
+
+/** SkeletonTextProps */
+export type SkeletonTextProps = TextProps & {
+  done?: boolean
+}
+
+const SkeletonTextOverlay = styled(SkeletonBox)`
+  position: absolute;
+  top: 50%;
+  left: 0;
+  width: 100%;
+  height: 85%;
+  transform: translateY(-50%);
+`
+
+/** SkeletonText */
+export const SkeletonText: React.FC<SkeletonTextProps> = ({
+  children,
+  done,
+  ...rest
+}) => {
+  const [borderProps, notBorderProps] = splitBorderProps(rest)
+
+  return (
+    <Text aria-busy={!done} {...notBorderProps}>
+      <Box
+        as="span"
+        display="inline-flex"
+        position="relative"
+        aria-hidden="true"
+      >
+        {children}
+
+        <SkeletonTextOverlay {...borderProps} />
+      </Box>
+    </Text>
+  )
+}
