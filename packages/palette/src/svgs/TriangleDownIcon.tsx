57a429aae6a9a5838cb76394023c18cfc246fa5c
Christopher Pappas <damassi.pappas@gmail.com>
2020-10-26 15:04:25 -0700

    feat: Add blue100 color

--- packages/palette/src/svgs/TriangleDownIcon.tsx
+++ packages/palette/src/svgs/TriangleDownIcon.tsx
@@ -13,7 +13,7 @@ export const TriangleDownIcon: React.SFC<IconProps> = ({
       <G width="11" height="6" viewBox="0 0 11 6" fill="none">
         <Path
           fillRule="evenodd"
-          clip-rule="evenodd"
+          clipRule="evenodd"
           d="M5.5 6L0 0L11 0L5.5 6Z"
           fill={color(props.fill)}
         />
