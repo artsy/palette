e73a66140eff8930ff7de3de5e6a8f3f40f9b210
dzucconi <mail@damonzucconi.com>
2021-01-14 14:35:03 -0500

    chore(button): clean up stories

--- packages/palette/src/elements/Button/Button.story.tsx
+++ packages/palette/src/elements/Button/Button.story.tsx
@@ -1,97 +1,47 @@
 import { storiesOf } from "@storybook/react"
 import React from "react"
-import styled from "styled-components"
 import { Box } from "../Box"
 import { Join } from "../Join"
 import { Spacer } from "../Spacer"
-import { Button } from "./Button"
-
-const StyledButton = styled(Button)`
-  border: 2px solid red;
-`
+import { Button, ButtonSize } from "./Button"
 
 storiesOf("Components/Button", module)
-  .add("small", () => {
+  .add("sizes", () => {
     return (
       <>
-        <Button size="small" variant="primaryBlack" mr={1}>
-          primaryBlack
-        </Button>
-        <Button size="small" variant="primaryWhite" mr={1}>
-          primaryWhite
-        </Button>
-        <Button size="small" variant="secondaryGray" mr={1}>
-          secondaryGray
-        </Button>
-        <Button size="small" variant="secondaryOutline" mr={1}>
-          secondaryOutline
-        </Button>
-        <Button size="small" variant="noOutline">
-          noOutline
-        </Button>
-      </>
-    )
-  })
-  .add("medium", () => {
-    return (
-      <>
-        <Button size="medium" variant="primaryBlack" mr={1}>
-          primaryBlack
-        </Button>
-        <Button size="medium" variant="primaryWhite" mr={1}>
-          primaryWhite
-        </Button>
-        <Button size="medium" variant="secondaryGray" mr={1}>
-          secondaryGray
-        </Button>
-        <Button size="medium" variant="secondaryOutline" mr={1}>
-          secondaryOutline
-        </Button>
-        <Button size="medium" variant="noOutline">
-          noOutline
-        </Button>
-      </>
-    )
-  })
-  .add("large", () => {
-    return (
-      <>
-        <Button size="large" variant="primaryBlack" mr={1}>
-          primaryBlack
-        </Button>
-        <Button size="large" variant="primaryWhite" mr={1}>
-          primaryWhite
-        </Button>
-        <Button size="large" variant="secondaryGray" mr={1}>
-          secondaryGray
-        </Button>
-        <Button size="large" variant="secondaryOutline" mr={1}>
-          secondaryOutline
-        </Button>
-        <Button size="large" variant="noOutline">
-          noOutline
-        </Button>
-      </>
-    )
-  })
-  .add("styled(Button)", () => {
-    return (
-      <>
-        <StyledButton size="large" variant="primaryBlack" mr={1}>
-          primaryBlack
-        </StyledButton>
-        <StyledButton size="large" variant="primaryWhite" mr={1}>
-          primaryWhite
-        </StyledButton>
-        <StyledButton size="large" variant="secondaryGray" mr={1}>
-          secondaryGray
-        </StyledButton>
-        <StyledButton size="large" variant="secondaryOutline" mr={1}>
-          secondaryOutline
-        </StyledButton>
-        <StyledButton size="large" variant="noOutline">
-          noOutline
-        </StyledButton>
+        {(["small", "medium", "large"] as ButtonSize[]).map(size => {
+          return (
+            <Box
+              key={size}
+              m={2}
+              p={2}
+              border="1px solid"
+              borderColor="black10"
+            >
+              <Join separator={<Spacer my={1} />}>
+                <Button size={size} variant="primaryBlack">
+                  primaryBlack
+                </Button>
+
+                <Button size={size} variant="primaryWhite">
+                  primaryWhite
+                </Button>
+
+                <Button size={size} variant="secondaryGray">
+                  secondaryGray
+                </Button>
+
+                <Button size={size} variant="secondaryOutline">
+                  secondaryOutline
+                </Button>
+
+                <Button size={size} variant="noOutline">
+                  noOutline
+                </Button>
+              </Join>
+            </Box>
+          )
+        })}
       </>
     )
   })
