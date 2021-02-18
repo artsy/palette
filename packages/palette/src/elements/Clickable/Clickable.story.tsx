55bd9cd66d404f4a1d4880a714ac0eaa43c7137a
dzucconi <mail@damonzucconi.com>
2021-02-18 17:00:41 -0500

    fix(clickable): sets default type to button

--- packages/palette/src/elements/Clickable/Clickable.story.tsx
+++ packages/palette/src/elements/Clickable/Clickable.story.tsx
@@ -1,6 +1,7 @@
 import { action } from "@storybook/addon-actions"
-import React from "react"
-import { Sans } from "../Typography"
+import React, { useState } from "react"
+import { Box } from "../Box"
+import { Text } from "../Text"
 import { Clickable } from "./Clickable"
 
 export default {
@@ -10,8 +11,41 @@ export default {
 export const Default = () => {
   return (
     <Clickable onClick={action("onClick")} p={3}>
-      <Sans size="4">Click</Sans>
-      <Sans size="1">or click</Sans>
+      <Text>Click</Text>
+      <Text variant="small">or click</Text>
     </Clickable>
   )
+}
+
+export const Submit = () => {
+  const [isSubmitted, setSubmitted] = useState(false)
+
+  return (
+    <form
+      onSubmit={(e) => {
+        e.preventDefault()
+        setSubmitted(true)
+
+        setTimeout(() => {
+          setSubmitted(false)
+        }, 1000)
+      }}
+    >
+      <Text>submitted? {isSubmitted ? "yes" : "no"}</Text>
+
+      <Box mt={1}>
+        <Clickable border="1px solid" p={2} type="button" mr={1}>
+          type="button"
+        </Clickable>
+
+        <Clickable border="1px solid" p={2} type="submit">
+          type="submit"
+        </Clickable>
+      </Box>
+    </form>
+  )
+}
+
+Submit.story = {
+  name: "Does not submit forms by default",
 }
