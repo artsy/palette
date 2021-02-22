85fe2b83eb5df853630dc6dd89dad9d5996ce083
dzucconi <mail@damonzucconi.com>
2021-02-22 16:09:01 -0500

    refactor(toggle): converts to functional component; converts stories

--- packages/palette/src/elements/Toggle/Toggle.story.tsx
+++ packages/palette/src/elements/Toggle/Toggle.story.tsx
@@ -1,8 +1,9 @@
 import React from "react"
+import { States } from "storybook-states"
 import { Box } from "../Box"
 import { Link } from "../Link"
 import { Text } from "../Text"
-import { Toggle } from "./Toggle"
+import { Toggle, ToggleProps } from "./Toggle"
 
 const SecondaryAction = () => {
   return (
@@ -21,81 +22,28 @@ export default {
   title: "Components/Toggle",
 }
 
-export const _Toggle = () => {
+export const Default = () => {
   return (
-    <Box width="350px">
-      <Toggle label="Test" expanded>
-        <h1>Hello world</h1>
+    <States<ToggleProps>
+      states={[
+        {},
+        { expanded: true },
+        { expanded: true, disabled: false },
+        { renderSecondaryAction: SecondaryAction },
+        { renderSecondaryAction: SecondaryAction, disabled: true },
+        {
+          label: (
+            <>
+              <Text variant="mediumText">Heading</Text>
+              <Text variant="text">Subheading</Text>
+            </>
+          ),
+        },
+      ]}
+    >
+      <Toggle label="Example">
+        <Text>Expanded content</Text>
       </Toggle>
-    </Box>
+    </States>
   )
-}
-
-export const ToggleDisabled = () => {
-  return (
-    <Box width="350px">
-      <Toggle label="Test" expanded disabled>
-        <h1>Hello world</h1>
-      </Toggle>
-    </Box>
-  )
-}
-
-ToggleDisabled.story = {
-  name: "Toggle disabled",
-}
-
-export const ToggleWithSecondaryAction = () => {
-  return (
-    <Box width="350px">
-      <Toggle label="Test" expanded renderSecondaryAction={SecondaryAction}>
-        <h1>Hello world</h1>
-      </Toggle>
-    </Box>
-  )
-}
-
-ToggleWithSecondaryAction.story = {
-  name: "Toggle with secondary action",
-}
-
-export const ToggleDisabledWithSecondaryAction = () => {
-  return (
-    <Box width="350px">
-      <Toggle
-        label="Test"
-        expanded
-        disabled
-        renderSecondaryAction={SecondaryAction}
-      >
-        <h1>Hello world</h1>
-      </Toggle>
-    </Box>
-  )
-}
-
-ToggleDisabledWithSecondaryAction.story = {
-  name: "Toggle disabled with secondary action",
-}
-
-export const ToggleWithAComponentAsTheLabel = () => {
-  return (
-    <Box width="350px">
-      <Toggle
-        label={
-          <>
-            <Text variant="mediumText">Heading</Text>
-            <Text variant="text">Subheading</Text>
-          </>
-        }
-        expanded
-      >
-        <h1>Hello world</h1>
-      </Toggle>
-    </Box>
-  )
-}
-
-ToggleWithAComponentAsTheLabel.story = {
-  name: "Toggle with a component as the label",
 }
--- packages/palette/src/elements/Toggle/Toggle.tsx
+++ packages/palette/src/elements/Toggle/Toggle.tsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from "react"
 import styled from "styled-components"
 import { space, SpaceProps } from "styled-system"
 import { ChevronIcon } from "../../svgs/ChevronIcon"
@@ -28,84 +28,63 @@ export interface ToggleState {
 }
 
 /** A toggle component used to show / hide / expand content  */
-export class Toggle extends React.Component<ToggleProps> {
-  static defaultProps = {
-    textSize: "2",
-    chevronSize: 12,
-  }
+export const Toggle: React.FC<ToggleProps> = ({
+  label,
+  textSize = "2",
+  chevronSize = 12,
+  expanded: defaultExpanded,
+  disabled,
+  children,
+  renderSecondaryAction,
+}) => {
+  const [expanded, setExpanded] = useState(defaultExpanded)
 
-  state = {
-    expanded: false,
-    disabled: false,
-  }
-
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      ...props,
+  const toggleExpand = () => {
+    if (!disabled) {
+      setExpanded((prevExpanded) => !prevExpanded)
     }
   }
 
-  toggleExpand = () => {
-    if (!this.props.disabled) {
-      this.setState({
-        expanded: !this.state.expanded,
-      })
-    }
-  }
+  return (
+    <Flex width="100%" flexDirection="column" pb={2}>
+      <Separator mb={2} />
+      <Header onClick={toggleExpand} disabled={disabled}>
+        <Flex justifyContent="space-between" alignItems="center">
+          {typeof label === "string" ? (
+            <Sans
+              size={textSize as SansSize}
+              weight="medium"
+              color="black100"
+              my={0.5}
+            >
+              {label}
+            </Sans>
+          ) : (
+            label
+          )}
 
-  render() {
-    const { disabled, expanded } = this.state
-    const {
-      children,
-      chevronSize,
-      label,
-      textSize,
-      renderSecondaryAction,
-    } = this.props
+          <Flex justifyContent="right" alignItems="center">
+            {renderSecondaryAction &&
+              renderSecondaryAction({ disabled, expanded, textSize })}
 
-    const labelComponent =
-      typeof label === "string" ? (
-        <Sans
-          size={textSize as SansSize}
-          weight="medium"
-          color="black100"
-          my={0.5}
-        >
-          {label}
-        </Sans>
-      ) : (
-        label
-      )
-
-    return (
-      <Flex width="100%" flexDirection="column" pb={2}>
-        <Separator mb={2} />
-        <Header onClick={this.toggleExpand} disabled={disabled}>
-          <Flex justifyContent="space-between" alignItems="center">
-            {labelComponent}
-            <Flex justifyContent="right" alignItems="center">
-              {renderSecondaryAction &&
-                renderSecondaryAction({ disabled, expanded, textSize })}
-              <ChevronIcon
-                style={{ visibility: disabled ? "hidden" : "visible" }}
-                direction={expanded ? "up" : "down"}
-                width={chevronSize}
-                height={chevronSize}
-                ml={1}
-              />
-            </Flex>
+            <ChevronIcon
+              style={{ visibility: disabled ? "hidden" : "visible" }}
+              direction={expanded ? "up" : "down"}
+              width={chevronSize}
+              height={chevronSize}
+              ml={1}
+            />
           </Flex>
-        </Header>
-        {expanded && children && (
-          <Flex flexDirection="column" alignItems="left">
-            {children}
-          </Flex>
-        )}
-      </Flex>
-    )
-  }
+        </Flex>
+      </Header>
+
+      {expanded && children && (
+        <Flex flexDirection="column" alignItems="left">
+          {children}
+        </Flex>
+      )}
+    </Flex>
+  )
 }
 
 const Header = styled.div<ToggleProps & SpaceProps>`
