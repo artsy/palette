d089cb4ad538c5c52c97413b285165945e91a3a3
dzucconi <mail@damonzucconi.com>
2021-03-03 14:04:47 -0500

    chore(stories): pulls over story updates from radio button

--- packages/palette/src/elements/RadioGroup/RadioGroup.story.tsx
+++ packages/palette/src/elements/RadioGroup/RadioGroup.story.tsx
@@ -1,63 +1,61 @@
 import { action } from "@storybook/addon-actions"
 import React, { useState } from "react"
-import { BorderBox } from "../BorderBox"
+import { States } from "storybook-states"
+import { Button } from "../Button"
 import { Radio } from "../Radio/Radio"
-import { RadioGroup } from "./RadioGroup"
+import { Text } from "../Text"
+import { RadioGroup, RadioGroupProps } from "./RadioGroup"
 
 export default {
   title: "Components/RadioGroup",
 }
 
+export const Default = () => {
+  return (
+    <States<Partial<RadioGroupProps>>
+      states={[
+        {},
+        { defaultValue: "Aural" },
+        { deselectable: true },
+        { disabled: true },
+        { disabled: true, disabledText: "Reason for disabled" },
+      ]}
+    >
+      <RadioGroup onSelect={action("onSelect")}>
+        {["Visual", "Linguistic", "Spatial", "Aural", "Gestural"].map(
+          (value) => {
+            return (
+              <Radio key={value} value={value} label={<Text>{value}</Text>} />
+            )
+          }
+        )}
+      </RadioGroup>
+    </States>
+  )
+}
+
 export const WithDefaultValue = () => {
-  const RadioGroupToggle = () => {
-    const [defaultValue, setValue] = useState("PICKUP")
-    return (
-      <>
-        <BorderBox
-          mb={2}
-          style={{ cursor: "pointer" }}
-          onClick={() => {
-            setValue(defaultValue === "PICKUP" ? "SHIP" : "PICKUP")
-          }}
-        >
-          Toggle default value: {defaultValue}
-        </BorderBox>
-        <RadioGroup defaultValue={defaultValue}>
-          <Radio
-            value="SHIP"
-            label="Provide shipping address"
-            onSelect={action("onSelect")}
-          />
-          <Radio
-            value="PICKUP"
-            label="Arrange for pickup"
-            onSelect={action("onSelect")}
-          />
-        </RadioGroup>
-      </>
-    )
-  }
-  return <RadioGroupToggle />
+  const [defaultValue, setValue] = useState("PICKUP")
+
+  return (
+    <>
+      <Button
+        mb={2}
+        onClick={() => {
+          setValue(defaultValue === "PICKUP" ? "SHIP" : "PICKUP")
+        }}
+      >
+        Toggle default value: {defaultValue}
+      </Button>
+
+      <RadioGroup defaultValue={defaultValue} onSelect={action("onSelect")}>
+        <Radio value="SHIP" label="Provide shipping address" />
+        <Radio value="PICKUP" label="Arrange for pickup" />
+      </RadioGroup>
+    </>
+  )
 }
 
 WithDefaultValue.story = {
   name: "With default value",
-}
-
-export const Deselectable = () => {
-  return (
-    <RadioGroup defaultValue="SHIP" deselectable>
-      <Radio value="SHIP" label="Provide shipping address" />
-      <Radio value="PICKUP" label="Arrange for pickup" />
-    </RadioGroup>
-  )
-}
-
-export const Disabled = () => {
-  return (
-    <RadioGroup defaultValue="SHIP" disabled disabledText="disabled text">
-      <Radio value="SHIP" label="Provide shipping address" />
-      <Radio value="PICKUP" label="Arrange for pickup" />
-    </RadioGroup>
-  )
 }
