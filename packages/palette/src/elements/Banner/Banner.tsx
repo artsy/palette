d3f9298b23c9d9dfe0385280bbe74a5bf1133ca6
dzucconi <mail@damonzucconi.com>
2021-03-04 14:25:51 -0500

    feat(banner): v3 banner

--- packages/palette/src/elements/Banner/Banner.tsx
+++ packages/palette/src/elements/Banner/Banner.tsx
@@ -1,79 +1,81 @@
-import React from "react"
+import React, { useState } from "react"
 import styled from "styled-components"
+import { variant } from "styled-system"
 import { CloseIcon } from "../../svgs/CloseIcon"
-import { Box } from "../Box"
-import { Flex } from "../Flex"
-import { Sans } from "../Typography"
+import { useThemeConfig } from "../../Theme"
+import { Clickable } from "../Clickable"
+import { Flex, FlexProps } from "../Flex"
+import { Text, TextVariant } from "../Text"
 
-const Target = styled(Flex)`
-  padding-left: 10px;
-  cursor: pointer;
+const VARIANTS = {
+  defaultLight: {
+    backgroundColor: "black10",
+    color: "black100",
+  },
+  defaultDark: {
+    backgroundColor: "black100",
+    color: "white100",
+  },
+  success: {
+    backgroundColor: "green100",
+    color: "white100",
+  },
+  error: {
+    backgroundColor: "red100",
+    color: "white100",
+  },
+  brand: {
+    backgroundColor: "brand",
+    color: "white100",
+  },
+}
 
-  svg {
-    display: block;
+const Container = styled(Flex)`
+  ${variant({ variants: VARIANTS })}
+`
+
+export interface BannerProps extends FlexProps {
+  variant?: keyof typeof VARIANTS
+  dismissable?: boolean
+}
+
+/** A banner */
+export const Banner: React.FC<BannerProps> = ({
+  dismissable = false,
+  children,
+  ...rest
+}) => {
+  const size: TextVariant = useThemeConfig({ v2: "small", v3: "sm" })
+
+  const [dismissed, setDismissed] = useState(false)
+
+  const handleClick = () => {
+    setDismissed(true)
   }
-`
 
-const Wrapper = styled(Box)`
-  transition: background-color 250ms linear;
-  display: flex;
-`
+  if (dismissed) return null
 
-const TextWrapper = styled(Flex)`
-  width: 100%;
-  text-align: center;
-  align-items: center;
-  justify-content: center;
-`
-
-const CloseButton = ({ onClick }) => {
   return (
-    <Target onClick={onClick} alignItems="center">
-      <CloseIcon fill="white100" />
-    </Target>
+    <Container p={1} {...rest}>
+      <Text variant={size} textAlign="center" flex="1">
+        {children}
+      </Text>
+
+      {dismissable && (
+        <Clickable
+          pl={1}
+          display="flex"
+          alignItems="center"
+          color="currentColor"
+          onClick={handleClick}
+        >
+          <CloseIcon style={{ fill: "currentcolor" }} />
+        </Clickable>
+      )}
+    </Container>
   )
 }
 
-export interface BannerProps {
-  dismissable: boolean
-  message?: string
-  backgroundColor: string
-  textColor: string
-}
-
-/**
- * A banner
- */
-export class Banner extends React.Component<BannerProps> {
-  static defaultProps = {
-    dismissable: false,
-    backgroundColor: "red100",
-    textColor: "white100",
-  }
-
-  state = {
-    dismissed: false,
-  }
-
-  handleCloseClick = () => {
-    this.setState({ dismissed: true })
-  }
-
-  render() {
-    if (this.state.dismissed) return null
-    const showCloseButton = this.props.dismissable
-
-    return (
-      <Wrapper
-        bg={this.props.backgroundColor}
-        color={this.props.textColor}
-        p={1}
-      >
-        <TextWrapper>
-          <Sans size="2">{this.props.children || this.props.message}</Sans>
-        </TextWrapper>
-        {showCloseButton && <CloseButton onClick={this.handleCloseClick} />}
-      </Wrapper>
-    )
-  }
+Banner.defaultProps = {
+  variant: "defaultLight",
 }
