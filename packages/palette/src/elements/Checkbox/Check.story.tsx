b3aadc44fe868b1ccf1b863f4ee5f1068ef674ba
dzucconi <mail@damonzucconi.com>
2021-03-29 14:24:29 -0400

    feat(checkbox): implements v3 styles

---
+++ packages/palette/src/elements/Checkbox/Check.story.tsx
@@ -0,0 +1,26 @@
+import React from "react"
+import { States } from "storybook-states"
+import { Check } from "./Check"
+
+export default {
+  title: "Components/Check",
+}
+
+export const Default = () => {
+  return (
+    <States
+      states={[
+        {},
+        { selected: true },
+        { hover: true },
+        { hover: true, selected: true },
+        { disabled: true },
+        { disabled: true, selected: true },
+        { error: true },
+        { error: true, selected: true },
+      ]}
+    >
+      <Check />
+    </States>
+  )
+}
---
+++ packages/palette/src/elements/Checkbox/tokens/v2.ts
@@ -0,0 +1,69 @@
+import { themeGet } from "@styled-system/theme-get"
+import { css } from "styled-components"
+
+export const CHECK_STATES = {
+  default: {
+    resting: css`
+      background-color: ${themeGet("colors.white100")};
+      border-color: ${themeGet("colors.black10")};
+      color: ${themeGet("colors.white100")};
+    `,
+    selected: css`
+      background-color: ${themeGet("colors.black100")};
+      border-color: ${themeGet("colors.black100")};
+      color: ${themeGet("colors.white100")};
+    `,
+  },
+  disabled: {
+    resting: css`
+      background-color: ${themeGet("colors.black5")};
+      border-color: ${themeGet("colors.black10")};
+      color: ${themeGet("colors.black5")};
+    `,
+    selected: css`
+      background-color: ${themeGet("colors.black30")};
+      border-color: ${themeGet("colors.black30")};
+      color: ${themeGet("colors.white100")};
+    `,
+  },
+  hover: {
+    resting: css`
+      background-color: ${themeGet("colors.black10")};
+      border-color: ${themeGet("colors.black10")};
+      color: ${themeGet("colors.white100")};
+    `,
+    selected: css`
+      background-color: ${themeGet("colors.black100")};
+      border-color: ${themeGet("colors.black100")};
+      color: ${themeGet("colors.white100")};
+    `,
+  },
+  error: {
+    resting: css`
+      background-color: ${themeGet("colors.white100")};
+      border-color: ${themeGet("colors.red100")};
+      color: ${themeGet("colors.white100")};
+    `,
+    selected: css`
+      background-color: ${themeGet("colors.black100")};
+      border-color: ${themeGet("colors.black100")};
+      color: ${themeGet("colors.white100")};
+    `,
+  },
+} as const
+
+export const CHECKBOX_STATES = {
+  default: css`
+    color: ${themeGet("colors.black100")};
+  `,
+  hover: css`
+    color: ${themeGet("colors.black100")};
+  `,
+  disabled: css`
+    pointer-events: none;
+    color: ${themeGet("colors.black30")};
+  `,
+  error: css`
+    color: ${themeGet("colors.red100")};
+  `,
+} as const
