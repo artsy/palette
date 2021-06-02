8278ebe79eb6472edc424dccbd25d0ce12684c36
Pavlos Vinieratos <pvinis@gmail.com>
2021-06-02 17:23:57 +0300

    ugh

--- packages/palette/src/svgs/DecreaseIcon.tsx
+++ packages/palette/src/svgs/DecreaseIcon.tsx
@@ -1,8 +1,18 @@
 import React, { FC } from "react"
-import { IconProps } from "./Icon"
-import { IncreaseIcon } from "./IncreaseIcon"
+import { color } from "../helpers"
+import { Icon, IconProps, Path, Title, G } from "./Icon"
 
 export const DecreaseIcon: FC<IconProps> = ({
   title = "Decrease",
   ...props
-}) => <IncreaseIcon {...props} title="Decrease" transform="rotate(180, 5, 5)" />
+}) => (
+  <Icon {...props} viewBox="0 0 10 10">
+    <Title>{title}</Title>
+    <G transform="rotate(180, 5, 5)">
+      <Path
+        d="M0 4.92701L0.927644 5.83942L4.34137 2.31752V10H5.67718V2.31752L9.07236 5.83942L10 4.92701L5.00928 0L0 4.92701Z"
+        fill={color(props.fill)}
+      />
+    </G>
+  </Icon>
+)
