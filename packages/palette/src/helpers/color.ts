fe3d5ba77b95faa2b9c3f019e3a2c83c0419fe05
dzucconi <mail@damonzucconi.com>
2021-08-02 10:46:22 -0400

    chore(space,color): deprecates space & color functions

--- packages/palette/src/helpers/color.ts
+++ packages/palette/src/helpers/color.ts
@@ -3,5 +3,7 @@ import { Color, themeProps } from "../Theme"
 /**
  * A helper to easily access colors when not in a styled-components or
  * styled-systems context.
+ *
+ * @deprecated use component `color` or `borderColor` props, or `themeGet('colors.colorName')`
  */
 export const color = (colorKey: Color) => themeProps.colors[colorKey]
--- packages/palette/src/helpers/space.ts
+++ packages/palette/src/helpers/space.ts
@@ -3,6 +3,8 @@ import { SpacingUnit, themeProps } from "../Theme"
 /**
  * A helper to easily access space values when not in a styled-components or
  * styled-systems context.
+ *
+ * @deprecated use component spacing props, or `themeGet('space.n')`
  */
 export const space = (spaceKey: SpacingUnit): number => {
   return parseInt(themeProps.space[spaceKey], 10)
