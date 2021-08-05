2620be860ba5dd629125d2244c64c3b0ccb5205c
Christopher Pappas <damassi.pappas@gmail.com>
2021-08-05 14:35:26 -0700

    copy url on header click

--- packages/palette-docs/src/utils/useScrollToSection.ts
+++ packages/palette-docs/src/utils/useScrollToSection.ts
@@ -1,7 +1,10 @@
 import { useEffect } from "react"
+import { copyStringToClipboard } from "./copyStringToClipboard"
 
 export function useScrollToHash({ hash, contentRef }) {
   useEffect(() => {
+    copyStringToClipboard(window.location.href)
+
     if (!hash) return
     const anchor = document.querySelectorAll(`[href="${hash}"]`)[0]
     const offset =
