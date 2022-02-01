31bda91ff646f3712d28d475e35fecbbacab2417
Lily Pace <lilyfpace@gmail.com>
2022-02-01 16:20:44 -0500

    scroll is working

---
+++ packages/palette/src/elements/Ribbon/Ribbon.story.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+import { Ribbon } from "./Ribbon"
+
+export default {
+  title: "Components/Ribbon",
+}
+
+export const Default = () => {
+  return <Ribbon ribbonText="Black Owned" repetitionCount={6} />
+}
