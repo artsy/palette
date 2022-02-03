9b40e34d24309f6e25ab904d6f203200f21c04bd
dzucconi <mail@damonzucconi.com>
2022-02-03 10:45:27 -0500

    fix: deprecates borderbox

--- packages/palette/src/elements/BorderBox/BorderBox.tsx
+++ packages/palette/src/elements/BorderBox/BorderBox.tsx
@@ -5,6 +5,8 @@ import { BorderBoxBase, BorderBoxProps } from "./BorderBoxBase"
 /**
  * A `div` that has a common border and padding set by default, with an optional
  * `hover` property for visually focusing content.
+ * @deprecated: Use `Box` along with props e.g. `border="1px solid" borderColor="black10"`,
+ * as needed.
  */
 export const BorderBox = styled(BorderBoxBase)<BorderBoxProps>`
   ${({ hover }) =>
