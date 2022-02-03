ba259a1b53ef5217639ccba201dd2a5a344a83b8
dzucconi <mail@damonzucconi.com>
2022-02-03 12:31:14 -0500

    docs: updates spacer example

--- packages/palette/src/elements/Spacer/Spacer.tsx
+++ packages/palette/src/elements/Spacer/Spacer.tsx
@@ -2,12 +2,12 @@ import React from "react"
 import { Box, BoxProps } from "../../elements/Box"
 
 /** Spacer implements Box */
-export type SpacerProps = BoxProps
+export type SpacerProps = BoxProps & React.HTMLAttributes<HTMLDivElement>
 
 /**
  * A component used to inject space where it's needed
  */
-export const Spacer: React.FC<SpacerProps & { id?: string }> = (props) => {
+export const Spacer: React.FC<SpacerProps> = (props) => {
   return <Box {...props} />
 }
 
