7e9e070b1b542b2bc3bf6f625481a76842161feb
dzucconi <mail@damonzucconi.com>
2022-02-04 17:08:34 -0500

    docs: cleans up icon/color docs

--- packages/palette-docs/src/components/ColorComponent.tsx
+++ packages/palette-docs/src/components/ColorComponent.tsx
@@ -6,10 +6,18 @@ export const ColorComponent = (props) => {
 
   return (
     <Flex mb={3}>
-      <Box width={50} height={50} bg={props.color} mr={1} borderRadius="50%" />
-      <Box>
+      <Box
+        width={50}
+        height={50}
+        bg={props.color}
+        mr={1}
+        borderRadius="50%"
+        border="1px solid"
+        borderColor="black10"
+      />
+      <Box display="flex" flexDirection="column" justifyContent="center">
         <Text variant="sm">{props.color}</Text>
-        <Text variant="md">
+        <Text variant="xs" color="black60">
           {theme.colors.hasOwnProperty(`${props.color}`)
             ? theme.colors[`${props.color}`]
             : ""}
--- packages/palette/src/svgs/_icons.story.tsx
+++ packages/palette/src/svgs/_icons.story.tsx
@@ -1,9 +1,19 @@
 import React from "react"
-import { Box } from "../elements/Box/Box"
-import { Flex } from "../elements/Flex/Flex"
-import { Separator } from "../elements/Separator/Separator"
-import { Sans } from "../elements/Typography"
+import { Column, GridColumns } from "../elements/GridColumns"
+import { Separator } from "../elements/Separator"
+import { Text } from "../elements/Text/Text"
 import * as AllIcons from "./index"
+
+const BLOCKLIST = [
+  "ChevronIcon",
+  "Circle",
+  "CreditCardIcon",
+  "G",
+  "Icon",
+  "Path",
+  "Rect",
+  "Title",
+]
 
 export default {
   title: "Icons",
@@ -11,27 +21,28 @@ export default {
 
 export const _AllIcons = () => {
   return (
-    <Flex flexWrap="wrap" m={2}>
-      {Object.entries(AllIcons).map(([iconName, IconComponent], index) => {
-        const blocklist = ["ChevronIcon", "CreditCardIcon", "Icon"]
-        if (
-          !(typeof IconComponent === "function") ||
-          blocklist.some((icon) => icon === iconName)
-        ) {
-          return null
-        }
-        const size = "40px"
-        return (
-          <Box pr={4} pb={2} mb={4} key={index} width="20%">
-            <IconComponent width={size} height={size} />
-            <Separator my={1} />
-            <Sans size="3" weight="medium">
-              {iconName}
-            </Sans>
-          </Box>
-        )
-      })}
-    </Flex>
+    <GridColumns>
+      {Object.entries(AllIcons)
+        .sort()
+        .map(([name, IconComponent]) => {
+          if (
+            !(typeof IconComponent === "function") ||
+            BLOCKLIST.includes(name)
+          ) {
+            return null
+          }
+
+          return (
+            <Column key={name} span={2}>
+              <IconComponent width={40} height={40} />
+
+              <Separator my={1} />
+
+              <Text variant="xs">{name}</Text>
+            </Column>
+          )
+        })}
+    </GridColumns>
   )
 }
 
