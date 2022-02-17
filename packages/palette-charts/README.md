94d90df498bc7b0897b23075853dc5492e67aa54
Steven Hicks <steven.j.hicks@gmail.com>
2022-02-17 13:55:08 -0600

    feat: functioning storybooks for palette-charts

--- packages/palette-charts/README.md
+++ packages/palette-charts/README.md
@@ -5,3 +5,13 @@ Artsy's Design System
 ## What is Palette?
 
 Contains the charting libraries that are part of the `@artsy/palette` library.
+
+## Local development
+
+In the project root run the following:
+
+```sh
+$ yarn workspace @artsy/palette-charts storybook
+```
+
+This will compile Palette-charts and boot Storybooks for the charts only.
---
+++ packages/palette-charts/src/elements/DonutChart/DonutChart.story.tsx
@@ -0,0 +1,37 @@
+import React from "react"
+import { DonutChart } from "../DonutChart"
+import { Box } from "@artsy/palette"
+
+export default {
+  title: "Components/DonutChart",
+}
+
+export const SimpleDonut = () => {
+  return (
+    <Box width="500px">
+      <DonutChart
+        points={[
+          {
+            value: 567,
+            axisLabelX: "Sep 11",
+            tooltip: { title: "Sep 8", description: "100 clicks" },
+          },
+          {
+            value: 300,
+            axisLabelX: "Sep 12",
+            tooltip: { title: "Sep 12", description: "300 clicks" },
+          },
+          {
+            value: 800,
+            axisLabelX: "Sep 16",
+            tooltip: { title: "Sep 16", description: "800 clicks" },
+          },
+        ]}
+      />
+    </Box>
+  )
+}
+
+SimpleDonut.story = {
+  name: "simple donut",
+}
