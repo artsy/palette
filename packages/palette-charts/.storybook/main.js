f8260938a1d22a42183aabf92863453ce677bb93
Steven Hicks <steven.j.hicks@gmail.com>
2022-02-17 14:27:55 -0600

    feat: functioning storybooks for palette-charts (#1131)
    * feat: functioning storybooks for palette-charts
    
    * chore: lint

---
+++ packages/palette-charts/.storybook/main.js
@@ -0,0 +1,18 @@
+module.exports = {
+  stories: ["../src/**/*.story.(tsx|mdx)"],
+  addons: [
+    "@storybook/addon-actions",
+    "@storybook/addon-links",
+    "@storybook/addon-viewport",
+  ],
+  babel: async (options) => ({
+    ...options,
+    plugins: [["@babel/plugin-proposal-class-properties", { loose: true }]],
+  }),
+  core: {
+    builder: "webpack5",
+  },
+  features: {
+    storyStoreV7: true,
+  },
+}
