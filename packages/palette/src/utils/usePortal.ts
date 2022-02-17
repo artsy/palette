3d08d51201ff9c8e3db3bc53eab6c0a734f23560
dzucconi <mail@damonzucconi.com>
2022-02-17 17:28:10 -0500

    fix(useportal): makes hook safe for ssr

--- packages/palette/src/utils/usePortal.ts
+++ packages/palette/src/utils/usePortal.ts
@@ -2,12 +2,11 @@ import React, { useEffect, useRef, useCallback } from "react"
 import { createPortal as __createPortal__ } from "react-dom"
 
 export const usePortal = () => {
-  const appendEl = useRef(document.createElement("div"))
+  const appendEl = useRef<HTMLDivElement | null>(null)
 
   useEffect(() => {
-    if (appendEl.current === null) return
-
-    const el = appendEl.current
+    const el = appendEl.current ?? document.createElement("div")
+    appendEl.current = el
 
     document.body.appendChild(el)
 
@@ -17,7 +16,13 @@ export const usePortal = () => {
   }, [])
 
   const createPortal = useCallback((children: React.ReactNode) => {
-    return __createPortal__(children, appendEl.current)
+    if (typeof window === "undefined") return null
+
+    // May execute before effect runs and appendEl is set
+    const el = appendEl.current ?? document.createElement("div")
+    appendEl.current = el
+
+    return __createPortal__(children, el)
   }, [])
 
   return { createPortal }
