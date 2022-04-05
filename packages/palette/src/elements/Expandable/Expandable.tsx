0292430e7827b1b98883c53afce366594c1f5081
rquartararo <rquartararo@gmail.com>
2022-04-05 11:40:04 +0200

    feat: pass setExpanded to children

--- packages/palette/src/elements/Expandable/Expandable.tsx
+++ packages/palette/src/elements/Expandable/Expandable.tsx
@@ -81,7 +81,9 @@ export const Expandable: React.FC<ExpandableProps> = ({
         )}
       </Clickable>
 
-      {expanded && children}
+      {expanded && typeof children === "function"
+        ? children({ setExpanded, expanded })
+        : children}
     </Box>
   )
 }
