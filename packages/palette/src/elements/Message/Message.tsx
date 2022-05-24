22aa970d78b7d2b72c6cc05da4d2a213222abe32
dzucconi <mail@damonzucconi.com>
2022-05-24 15:50:04 -0400

    feat(message): upgrades message to 3.1

--- packages/palette/src/elements/Message/Message.tsx
+++ packages/palette/src/elements/Message/Message.tsx
@@ -6,7 +6,7 @@ import { useThemeConfig } from "../../Theme"
 import { Flex, FlexProps } from "../Flex"
 import { Text } from "../Text"
 
-const VARIANTS = {
+export const MESSAGE_VARIANTS = {
   default: {
     backgroundColor: "black10",
     color: "black100",
@@ -15,9 +15,17 @@ const VARIANTS = {
     backgroundColor: "blue10",
     color: "blue100",
   },
+  success: {
+    backgroundColor: "green10",
+    color: "green150",
+  },
+  alert: {
+    backgroundColor: "orange10",
+    color: "orange150",
+  },
   warning: {
-    backgroundColor: "copper10",
-    color: "copper100",
+    backgroundColor: "yellow10",
+    color: "yellow150",
   },
   error: {
     backgroundColor: "red10",
@@ -25,32 +33,41 @@ const VARIANTS = {
   },
 }
 
+export type MessageVariant = keyof typeof MESSAGE_VARIANTS
+
 export interface MessageProps extends FlexProps {
-  variant?: keyof typeof VARIANTS
-  title?: string
   children?: React.ReactNode
+  title?: string
+  variant?: MessageVariant
 }
 
 const Container = styled(Flex)<MessageProps>`
-  ${variant({ variants: VARIANTS })}
+  ${variant({ variants: MESSAGE_VARIANTS })}
   flex-direction: column;
 `
 
-/** A generic message window for displaying ZerStates, notices, errors, etc. */
+/**
+ * `Message` is used for in-line communication. These should fill the allotted
+ * container or space they are placed and key for temporal messages and
+ * comments within flows. Additionally, they can be used to highlight particular
+ * messaging within a specific section of a page or screen.
+ */
 export const Message: React.FC<MessageProps> = ({
   children,
   title,
+  variant,
   ...rest
 }) => {
-  const size: TextVariant = useThemeConfig({ v2: "text", v3: "sm" })
+  const size: TextVariant = useThemeConfig({ v2: "text", v3: "sm-display" })
+  const color = variant === "default" ? "black60" : "black100"
 
   return (
-    <Container p={2} {...rest}>
+    <Container p={2} variant={variant} {...rest}>
       <Text variant={size} color="currentColor">
         {title}
       </Text>
 
-      <Text variant={size} color="black100">
+      <Text variant={size} color={color}>
         {children}
       </Text>
     </Container>
