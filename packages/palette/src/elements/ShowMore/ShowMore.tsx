99addb72600c48691989676b2becf4a5e71385f0
Matt Dole <mdole7@gmail.com>
2022-08-30 17:54:15 +0200

    feat: strip default styles from ShowMore & apply from parent (#1202)

--- packages/palette/src/elements/ShowMore/ShowMore.tsx
+++ packages/palette/src/elements/ShowMore/ShowMore.tsx
@@ -1,11 +1,15 @@
 import React, { Children, isValidElement, useState } from "react"
-import { Clickable } from "../Clickable"
-import { Text } from "../Text"
+import { Clickable, ClickableProps, splitClickableProps } from "../Clickable"
+import { Text, TextProps } from "../Text"
 
-export interface ShowMoreProps {
+export interface ShowMoreProps
+  extends ClickableProps,
+    Pick<TextProps, "variant"> {
   children: React.ReactNode
   initial?: number
   expanded?: boolean
+  hideText?: string
+  showMoreText?: string
 }
 
 export const INITIAL_ITEMS_TO_SHOW = 6
@@ -14,10 +18,15 @@ export const ShowMore: React.FC<ShowMoreProps> = ({
   initial = INITIAL_ITEMS_TO_SHOW,
   children,
   expanded = false,
+  hideText = "Hide",
+  showMoreText = "Show more",
+  ...rest
 }) => {
   const [isExpanded, setExpanded] = useState(expanded)
   const nodes = Children.toArray(children).filter(isValidElement)
   const hasMore = nodes.length > initial
+
+  const [clickableProps, { variant }] = splitClickableProps(rest)
 
   return (
     <>
@@ -25,12 +34,10 @@ export const ShowMore: React.FC<ShowMoreProps> = ({
 
       {hasMore && (
         <Clickable
-          mt={1}
-          textDecoration="underline"
-          textAlign="left"
           onClick={() => setExpanded((visibility) => !visibility)}
+          {...clickableProps}
         >
-          <Text variant="xs">{isExpanded ? "Hide" : "Show more"}</Text>
+          <Text variant={variant}>{isExpanded ? hideText : showMoreText}</Text>
         </Clickable>
       )}
     </>
