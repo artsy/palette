61df98aba69c7a171bb67dddaf189664a0385248
dzucconi <mail@damonzucconi.com>
2022-12-08 12:56:35 -0500

    docs: updates readme

--- packages/palette/src/svgs/_icons.story.tsx
+++ packages/palette/src/svgs/_icons.story.tsx
@@ -1,8 +1,10 @@
 import React from "react"
+import { Message } from "../elements/Message"
 import { Column, GridColumns } from "../elements/GridColumns"
 import { Separator } from "../elements/Separator"
 import { Text } from "../elements/Text/Text"
 import * as AllIcons from "./index"
+import { Spacer } from "../elements/Spacer"
 
 const BLOCKLIST = [
   "ChevronIcon",
@@ -21,29 +23,49 @@ export default {
 
 export const _AllIcons = () => {
   return (
-    <GridColumns>
-      {Object.entries(AllIcons)
-        .sort()
-        .map(([name, IconComponent]) => {
-          if (
-            !(typeof IconComponent === "function") ||
-            BLOCKLIST.includes(name)
-          ) {
-            return null
-          }
+    <>
+      <Message variant="warning">
+        Please see{" "}
+        <a
+          href="https://github.com/artsy/icons"
+          target="_blank"
+          rel="noreferrer"
+        >
+          @artsy/icons
+        </a>{" "}
+        and{" "}
+        <a href="https://icons.artsy.net/" target="_blank" rel="noreferrer">
+          icons.artsy.net
+        </a>{" "}
+        for the latest icons. This page is deprecated and will be removed soon.
+      </Message>
 
-          return (
-            <Column key={name} span={2}>
-              {/* @ts-ignore */}
-              <IconComponent width={40} height={40} />
+      <Spacer y={2} />
 
-              <Separator my={1} />
+      <GridColumns>
+        {Object.entries(AllIcons)
+          .sort()
+          .map(([name, IconComponent]) => {
+            if (
+              !(typeof IconComponent === "function") ||
+              BLOCKLIST.includes(name)
+            ) {
+              return null
+            }
 
-              <Text variant="xs">{name}</Text>
-            </Column>
-          )
-        })}
-    </GridColumns>
+            return (
+              <Column key={name} span={2}>
+                {/* @ts-ignore */}
+                <IconComponent width={40} height={40} />
+
+                <Separator my={1} />
+
+                <Text variant="xs">{name}</Text>
+              </Column>
+            )
+          })}
+      </GridColumns>
+    </>
   )
 }
 
