48700e9773fe1eeb2e14cc232fe9061e7ac182a4
Sultan Al-Maari <sultan.al-maari@artsymail.com>
2023-01-30 14:56:33 +0100

    fix: update tests

--- packages/palette/src/elements/Input/__tests__/Input.test.tsx
+++ packages/palette/src/elements/Input/__tests__/Input.test.tsx
@@ -15,16 +15,24 @@ describe("Input", () => {
 
   it("returns a required input with title when provided both flags", () => {
     const wrapper = mount(<Input title="This is the title" required />)
-    expect(wrapper.text()).toContain("This is the title*")
+    expect(wrapper.text()).toContain("This is the title")
+    expect(wrapper.text()).toContain("*Required")
   })
 
   it("returns an input with description when provided", () => {
     const wrapper = mount(<Input description="This is the title" />)
+    expect(wrapper.text()).toContain("What is this?")
     expect(wrapper.text()).toContain("This is the title")
   })
 
   it("returns an input with error text when provided", () => {
     const wrapper = mount(<Input error="This is the error" />)
+    expect(wrapper.text()).toContain("This is the error")
+  })
+
+  it("hides required when the error message is shown", () => {
+    const wrapper = mount(<Input required error="This is the error" />)
+    expect(wrapper.text()).not.toContain("*Required")
     expect(wrapper.text()).toContain("This is the error")
   })
 })
