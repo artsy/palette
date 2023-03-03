54d85d369317014900885f907d50d8ae8f406d1c
Damon <mail@damonzucconi.com>
2023-03-03 12:21:49 -0500

    Revert "Revert "fix(usefocuslock): uses a more accurate method of determining focusable""

--- packages/palette/src/elements/Modal/__tests__/ModalBase.test.tsx
+++ packages/palette/src/elements/Modal/__tests__/ModalBase.test.tsx
@@ -3,9 +3,29 @@ import React, { useState } from "react"
 import { act } from "react-dom/test-utils"
 import { ModalBase } from "../ModalBase"
 
-const tick = () => new Promise((resolve) => setTimeout(resolve, 0))
+jest.mock("tabbable", () => {
+  const tabbable = jest.requireActual("tabbable")
+
+  return {
+    ...tabbable,
+    tabbable: (node, options) =>
+      tabbable.tabbable(node, { ...options, displayCheck: "none" }),
+    focusable: (node, options) =>
+      tabbable.focusable(node, { ...options, displayCheck: "none" }),
+    isFocusable: (node, options) =>
+      tabbable.isFocusable(node, { ...options, displayCheck: "none" }),
+    isTabbable: (node, options) =>
+      tabbable.isTabbable(node, { ...options, displayCheck: "none" }),
+  }
+})
+
+const flushPromiseQueue = () => new Promise((resolve) => setTimeout(resolve, 0))
 
 describe("ModalBase", () => {
+  beforeAll(() => {
+    jest.useFakeTimers()
+  })
+
   it("renders the children", () => {
     const wrapper = mount(<ModalBase onClose={jest.fn()}>content</ModalBase>)
     expect(wrapper.html()).toContain("content")
@@ -39,11 +59,15 @@ describe("ModalBase", () => {
           new KeyboardEvent("keydown", { key, shiftKey: shift })
         )
       })
-      await tick()
+
+      await flushPromiseQueue()
     }
 
     it("focuses the initial input", () => {
       const wrapper = mount(<Example />)
+
+      jest.runAllTimers()
+
       const input = wrapper.find("#foo")
       expect(input.getElement().props.id).toEqual("foo")
       expect(document.activeElement!.id).toEqual("foo")
@@ -52,29 +76,34 @@ describe("ModalBase", () => {
     it("manages the focus", async () => {
       mount(<Example />)
 
+      jest.runAllTimers()
+
       // Tabbing through
       expect(document.activeElement!.id).toEqual("foo")
-      await keydown("Tab", false)
+      keydown("Tab", false)
       expect(document.activeElement!.id).toEqual("bar")
-      await keydown("Tab", false)
+      keydown("Tab", false)
       expect(document.activeElement!.id).toEqual("baz")
       // Wraps around
-      await keydown("Tab", false)
+      keydown("Tab", false)
       expect(document.activeElement!.id).toEqual("foo")
       // Shift+tab backwards
-      await keydown("Tab", true)
+      keydown("Tab", true)
       expect(document.activeElement!.id).toEqual("baz")
-      await keydown("Tab", true)
+      keydown("Tab", true)
       expect(document.activeElement!.id).toEqual("bar")
-      await keydown("Tab", true)
+      keydown("Tab", true)
       expect(document.activeElement!.id).toEqual("foo")
-      await keydown("Tab", true)
+      keydown("Tab", true)
       // Wraps around
       expect(document.activeElement!.id).toEqual("baz")
     })
 
     it("returns focus to the previous element when closed", () => {
       const wrapper = mount(<Example />, { attachTo: document.body })
+
+      jest.runAllTimers()
+
       expect(document.activeElement!.id).toEqual("foo")
       wrapper.find("#open").simulate("click")
       expect(document.activeElement!.id).toEqual("qux")
