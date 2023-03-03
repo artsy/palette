c7c8ffa82aa0b614636cc3dfbf15693b266e8fc4
Damon <mail@damonzucconi.com>
2023-03-03 10:14:13 -0500

    Revert "fix(usefocuslock): uses a more accurate method of determining focusable"

--- packages/palette/src/utils/useFocusLock.ts
+++ packages/palette/src/utils/useFocusLock.ts
@@ -1,25 +1,34 @@
 import React, { useCallback, useEffect, useRef, useState } from "react"
 import { useCursor } from "use-cursor"
 import { useMutationObserver } from "./useMutationObserver"
-import { tabbable } from "tabbable"
+
+const FOCUSABLE_SELECTOR = [
+  "a[href]:not([tabindex='-1'])",
+  "area[href]:not([tabindex='-1'])",
+  "input:not([disabled]):not([tabindex='-1'])",
+  "select:not([disabled]):not([tabindex='-1'])",
+  "textarea:not([disabled]):not([tabindex='-1'])",
+  "button:not([disabled]):not([tabindex='-1'])",
+  '[tabindex="0"]',
+].join(", ")
 
 interface UseFocusLock {
   ref: React.MutableRefObject<HTMLElement | null>
   active?: boolean
 }
 
-type FocusableElement = HTMLElement | SVGElement
-
 /**
  * Locks focus within the given element
  */
 export const useFocusLock = ({ ref, active = true }: UseFocusLock) => {
-  const [focusableEls, setFocusableEls] = useState<FocusableElement[]>([])
+  const [focusableEls, setFocusableEls] = useState<HTMLElement[]>([])
 
   const updateFocusableEls = useCallback(() => {
     if (ref.current === null) return
 
-    setFocusableEls(tabbable(ref.current))
+    setFocusableEls(
+      Array.from(ref.current.querySelectorAll<HTMLElement>(FOCUSABLE_SELECTOR))
+    )
 
     // When `active` changes that typically means our target ref
     // is being inserted into the DOM, so we need to update the focusable elements.
@@ -27,16 +36,7 @@ export const useFocusLock = ({ ref, active = true }: UseFocusLock) => {
   }, [active])
 
   // Set initial focusable elements on mount
-  useEffect(() => {
-    // Wait for the next tick to ensure focusable elements are in the DOM
-    const timeout = setTimeout(() => {
-      updateFocusableEls()
-    }, 0)
-
-    return () => {
-      clearTimeout(timeout)
-    }
-  }, [updateFocusableEls])
+  useEffect(updateFocusableEls, [updateFocusableEls])
 
   const skipUpdateFocusRef = useRef(false)
 
