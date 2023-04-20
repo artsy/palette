59de90cf3ba93cca4e97e8d6c13255eb6880f0c9
dzucconi <mail@damonzucconi.com>
2023-04-20 16:07:27 -0400

    feat(hooks): exports useprevious

--- packages/palette/src/utils/usePrevious.ts
+++ packages/palette/src/utils/usePrevious.ts
@@ -4,7 +4,7 @@ import { useEffect, useRef } from "react"
  * usePrevious
  * Stores state or props in a ref to compare against new props or state
  */
-export function usePrevious(value) {
+export function usePrevious<T>(value: T) {
   const ref = useRef(value)
   useEffect(() => {
     ref.current = value
