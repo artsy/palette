71fb73deffb1e218482680c382f8b7e8ee275caa
dzucconi <mail@damonzucconi.com>
2023-04-28 17:29:04 -0400

    fix(filterselect): uses radio when single select

--- packages/palette/src/elements/FilterSelect/Components/FilterSelectResultItem.tsx
+++ packages/palette/src/elements/FilterSelect/Components/FilterSelectResultItem.tsx
@@ -1,27 +1,30 @@
 import React from "react"
 import { Checkbox } from "../../Checkbox"
 import { Item, useFilterSelectContext } from "./FilterSelectContext"
+import { Radio } from "../../Radio"
 
 export const FilterSelectResultItem: React.FC<Item> = (props) => {
   const {
     renderItemLabel,
     selectedItems,
     toggleSelectedItem,
+    multiselect,
   } = useFilterSelectContext()
 
   const isSelected = !!selectedItems.find(
     (item: Item) => item.value === props.value
   )
   const label = renderItemLabel ? renderItemLabel(props) : props.label
+  const Component = multiselect ? Checkbox : Radio
 
   return (
-    <Checkbox
+    <Component
       onSelect={() => toggleSelectedItem(props)}
       selected={isSelected}
       key={props.value}
       my={1}
     >
       {label}
-    </Checkbox>
+    </Component>
   )
 }
