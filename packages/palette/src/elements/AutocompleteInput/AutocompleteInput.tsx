45f11a978e2292b72babe679df5672b04a65792c
Sam <sam.raj@artsymail.com>
2023-05-16 15:32:06 +0200

    feat: close dropdown by clicking the footer

--- packages/palette/src/elements/AutocompleteInput/AutocompleteInput.tsx
+++ packages/palette/src/elements/AutocompleteInput/AutocompleteInput.tsx
@@ -23,6 +23,11 @@ import { VisuallyHidden } from "../VisuallyHidden"
 import { AutocompleteInputOption } from "./AutocompleteInputOption"
 import { AutocompleteInputOptionLabel } from "./AutocompleteInputOptionLabel"
 
+export interface FooterActions {
+  /** Call to hide dropdown */
+  onHide(): void
+}
+
 /** Base option type — can be expanded */
 export interface AutocompleteInputOptionType {
   text: string
@@ -61,7 +66,10 @@ export interface AutocompleteInputProps<T extends AutocompleteInputOptionType>
   defaultValue?: string
   loading?: boolean
   header?: React.ReactNode
-  footer?: React.ReactNode
+  // footer?: React.ReactNode
+  footer?:
+    | React.ReactNode
+    | ((dropdownActions: Pick<FooterActions, "onHide">) => void)
   /** on <enter> when no option is selected */
   onSubmit?(query: string): void
   /** on <click> or <enter> when an option is selected */
@@ -219,6 +227,10 @@ export const AutocompleteInput = <T extends AutocompleteInputOptionType>({
     onChange: handleFocusChange,
   })
 
+  const handleHide = useCallback(() => {
+    onClose?.()
+  }, [onClose])
+
   const handleInputKeydown = (event: React.KeyboardEvent<HTMLInputElement>) => {
     switch (event.key) {
       // Handle <Enter> when nothing is selected
@@ -366,7 +378,11 @@ export const AutocompleteInput = <T extends AutocompleteInputOptionType>({
             })}
           </AutocompleteInputOptions>
 
-          <div ref={footerRef}>{footer}</div>
+          <div ref={footerRef}>
+            {typeof footer === "function"
+              ? footer({ onHide: handleHide })
+              : footer}
+          </div>
         </AutocompleteInputDropdown>
       )}
 
