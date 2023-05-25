1b5d3070a42409983dd01d61a72e7a92e5f04e46
Carlos Alberto de Araujo Barreto <araujobarret@gmail.com>
2023-05-25 16:31:19 +0200

    feat(NX-3251): add drawer element (#1295)
    * feat: add drawer element
    
    * feat: move component out of Context, use animation api
    
    * feat: remove noise from logger when using react-focus-on
    
    * feat: improve typing
    
    * feat: make the drawer always present to use transitions
    
    * feat: remove unecessary transform from the container
    
    * feat: css cleanup
    
    * feat: remove class in favor of conditional style in the component

---
+++ packages/palette/src/elements/Drawer/Drawer.tsx
@@ -0,0 +1,100 @@
+import React, { FC } from "react"
+import { Box, Flex } from "@artsy/palette"
+import styled, { css } from "styled-components"
+import { zIndex } from "styled-system"
+import { FocusOn } from "react-focus-on"
+
+export interface DrawerProps {
+  open: boolean
+  anchor?: "left" | "right"
+  zIndex?: number
+  onClose?(): void
+}
+
+export const Drawer: FC<DrawerProps> = ({
+  children,
+  anchor = "right",
+  zIndex = DEFAULT_DRAWER_Z_INDEX,
+  open,
+  onClose,
+}) => {
+  return (
+    <Container
+      zIndex={zIndex}
+      anchor={anchor}
+      style={{
+        pointerEvents: open ? "auto" : "none",
+        transform: open ? "translateX(0)" : "none",
+      }}
+    >
+      <Focus onClickOutside={onClose} enabled={open} onEscapeKey={onClose}>
+        <Content
+          backgroundColor="white100"
+          height="100%"
+          width={["100%", "auto"]}
+          overflowX="hidden"
+          overflowY="scroll"
+          anchor={anchor}
+          zIndex={zIndex}
+          style={{
+            transform: open
+              ? "translateX(0)"
+              : `translateX(${anchor === "left" ? "-100%" : "100%"})`,
+          }}
+        >
+          {children}
+        </Content>
+      </Focus>
+
+      <Overlay
+        backgroundColor="black100"
+        height="100%"
+        display={["none", "flex"]}
+        onClick={onClose}
+        data-testid="drawer-overlay"
+        width="inherit"
+        style={{
+          opacity: open ? "0.5" : "0",
+        }}
+      />
+    </Container>
+  )
+}
+
+const DEFAULT_DRAWER_Z_INDEX = 1
+
+const Content = styled(Box)<Pick<DrawerProps, "anchor">>`
+  position: absolute;
+  top: 0;
+  transition: transform 200ms cubic-bezier(0.075, 0.82, 0.165, 1);
+  -webkit-overflow-scrolling: touch;
+
+  ${(props) => css`
+    ${props.anchor === "left" ? "left: 0;" : "right: 0;"}
+  `};
+`
+
+const Overlay = styled(Box)`
+  transition: opacity 150ms linear 50ms;
+`
+
+const Container = styled(Flex)<Pick<DrawerProps, "anchor">>`
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  ${zIndex}
+  position: fixed;
+
+  ${(props) => {
+    return css`
+      flex-direction: ${props.anchor === "left" ? "row" : "row-reverse"};
+    `
+  }}
+`
+
+const Focus = styled(FocusOn)`
+  height: 100%;
+  align-items: center;
+  justify-content: center;
+`
---
+++ packages/palette/src/elements/Drawer/__tests__/Drawer.test.tsx
@@ -0,0 +1,42 @@
+import React from "react"
+import { mount } from "enzyme"
+import { Drawer } from "../Drawer"
+import { Button } from "../../Button"
+import { Text } from "../../Text"
+
+const DrawerContent: React.FC = () => {
+  const [open, setOpen] = React.useState(false)
+
+  return (
+    <>
+      <Button onClick={() => setOpen(true)}>Open</Button>
+      <Drawer open={open} anchor="left" onClose={() => setOpen(false)}>
+        <Text>Drawer Content</Text>
+      </Drawer>
+    </>
+  )
+}
+
+describe("Drawer", () => {
+  jest.spyOn(console, "error").mockImplementation(() => null)
+
+  it("renders the drawer content when the open button is clicked", () => {
+    const wrapper = mount(<DrawerContent />)
+
+    expect(wrapper.find(Drawer).prop("open")).toBe(false)
+
+    wrapper.find(Button).simulate("click")
+
+    expect(wrapper.find(Drawer).prop("open")).toBe(true)
+  })
+
+  it("hides the drawer when the overlay is clicked", () => {
+    const wrapper = mount(<DrawerContent />)
+
+    wrapper.find("Button").simulate("click")
+    expect(wrapper.find(Drawer).prop("open")).toBe(true)
+
+    wrapper.find("[data-testid='drawer-overlay']").first().simulate("click")
+    expect(wrapper.find(Drawer).prop("open")).toBe(false)
+  })
+})
