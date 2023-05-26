9715a648c73847f47edd8c0c8e112d6c4bfb6ba0
Carlos Alberto de Araujo Barreto <araujobarret@gmail.com>
2023-05-26 13:41:04 +0200

    feat(Drawer): export Drawer element (#1300)
    * feat: export Drawer
    
    * fix: remove circular dep

--- packages/palette/src/elements/Drawer/Drawer.tsx
+++ packages/palette/src/elements/Drawer/Drawer.tsx
@@ -1,5 +1,6 @@
 import React, { FC } from "react"
-import { Box, Flex } from "@artsy/palette"
+import { Box } from "../Box"
+import { Flex } from "../Flex"
 import styled, { css } from "styled-components"
 import { zIndex } from "styled-system"
 import { FocusOn } from "react-focus-on"
