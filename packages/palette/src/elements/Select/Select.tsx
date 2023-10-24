7973161d6b036cd97ee4adfc619cb94a78c9c06c
Sultan Al-Maari <sltun2oo9@gmail.com>
2023-10-24 16:14:14 +0200

    fix(EMI-1373): Fix Select issues with Firefox (#1322)
    * add styles to Select element with JS
    
    * refactor the way to apply styles for firefox

--- packages/palette/src/elements/Select/Select.tsx
+++ packages/palette/src/elements/Select/Select.tsx
@@ -1,12 +1,16 @@
 import { themeGet } from "@styled-system/theme-get"
-import React, { forwardRef, ForwardRefExoticComponent, Ref } from "react"
+import React, {
+  forwardRef,
+  ForwardRefExoticComponent,
+  Ref,
+  useState,
+} from "react"
 import styled, { css } from "styled-components"
+import { RequiredField } from "../../shared/RequiredField"
 import { Box, BoxProps, splitBoxProps } from "../Box"
 import { Text } from "../Text"
-import { SELECT_STATES } from "./tokens"
 import { Tooltip } from "../Tooltip"
-import { RequiredField } from "../../shared/RequiredField"
-import { FORM_ELEMENT_TRANSITION } from "../../helpers"
+import { SELECT_STATES } from "./tokens"
 
 export interface Option {
   value: string
@@ -52,6 +56,10 @@ export const Select: ForwardRefExoticComponent<
     ref
   ) => {
     const [boxProps, selectProps] = splitBoxProps(rest)
+    // due to :has not available in Firefox yet, we need to add the styles to the label using JS
+    const [selectedOption, setSelectedOption] = useState(selected)
+    const [isFocused, setIsFocused] = useState(false)
+    const [isHovered, setIsHovered] = useState(false)
 
     return (
       <Box width="100%" {...boxProps}>
@@ -65,10 +73,15 @@ export const Select: ForwardRefExoticComponent<
 
         <Container
           disabled={!!disabled}
-          hover={!!hover}
+          hover={!!hover || isHovered}
           error={error!}
-          focus={!!focus}
+          focus={!!focus || isFocused}
           title={title}
+          onMouseEnter={() => setIsHovered(true)}
+          onMouseLeave={() => setIsHovered(false)}
+          onFocus={() => setIsFocused(true)}
+          onBlur={() => setIsFocused(false)}
+          optionSelected={!!selectedOption}
         >
           <select
             ref={ref as any}
@@ -78,6 +91,7 @@ export const Select: ForwardRefExoticComponent<
             value={selected}
             onChange={(event) => {
               onSelect && onSelect(event.target.value)
+              setSelectedOption(event.target.value)
             }}
             {...selectProps}
           >
@@ -93,6 +107,7 @@ export const Select: ForwardRefExoticComponent<
           {!!title && (
             <StyledLabel htmlFor={id}>
               {title}
+
               <span />
             </StyledLabel>
           )}
@@ -158,7 +173,8 @@ export const caretMixin = css`
 
 type ContainerProps = Required<
   Pick<SelectProps, "disabled" | "error" | "hover" | "focus">
->
+  // adding optionSelected here to use it locally without adding it to the Select's props
+> & { optionSelected: boolean }
 
 const Container = styled(Box)<ContainerProps>`
   position: relative;
@@ -199,10 +215,18 @@ const Container = styled(Box)<ContainerProps>`
           ${SELECT_STATES.disabled}
         }
 
-        &:not(:has(option[value=""]:checked)):not(:focus) {
+        &:not(:focus):not(:has(option[value=""]:checked)) {
           ${!(props.disabled || props.focus) && SELECT_STATES.completed}
           ${props.error && SELECT_STATES.error}
         }
+
+        // Firefox polyfill for :has
+        ${!props.focus &&
+        !!props.optionSelected &&
+        css`
+          ${!(props.disabled || props.focus) && SELECT_STATES.completed}
+          ${props.error && SELECT_STATES.error}
+        `}
 
         &:not(:focus):has(option[value=""]:checked) {
           ${props.title &&
@@ -210,6 +234,14 @@ const Container = styled(Box)<ContainerProps>`
             color: transparent;
           `}
         }
+
+        // Firefox polyfill for :has
+        ${!props.focus &&
+        !props.optionSelected &&
+        props.title &&
+        css`
+          color: transparent;
+        `}
       `
     }};
   }
