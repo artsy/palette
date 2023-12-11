d9d5d835d411768664814f8311d472aa50e673bc
Joel Rosenblatt <joelrosenblatt@mail.com>
2023-12-11 14:41:11 +0100

    chore: Port over intersection observer hook from force

---
+++ packages/palette-charts/src/elements/DataVis/useIntersectionObserver.ts
@@ -0,0 +1,72 @@
+import { RefObject, useEffect, useRef, useState } from "react"
+
+interface UseIntersectionObserverProperties {
+  ref?: RefObject<Element> | null
+  once?: boolean
+  options?: {
+    threshold: number | number[]
+    root?: Element
+    rootMargin?: string
+  }
+  onIntersection?: (entries: IntersectionObserverEntry[]) => void
+  onOffIntersection?: (entries: IntersectionObserverEntry[]) => void
+}
+
+// tslint:disable-next-line:completed-docs
+export const useIntersectionObserver = ({
+  once = true,
+  options = { threshold: 0 },
+  onIntersection,
+  onOffIntersection,
+}: UseIntersectionObserverProperties) => {
+  const ref = useRef<HTMLElement | null>(null)
+
+  const handleIntersect = (entries: IntersectionObserverEntry[]) => {
+    if (!observer) return
+
+    if (once) {
+      const hasIntersected = entries.some(
+        // tslint:disable-next-line:no-shadowed-variable
+        ({ isIntersecting }) => isIntersecting
+      )
+
+      if (hasIntersected) {
+        onIntersection?.(entries)
+        observer.disconnect()
+      } else {
+        onOffIntersection?.(entries)
+      }
+
+      return
+    }
+
+    const isIntersecting = entries[entries.length - 1].isIntersecting
+
+    if (!isIntersecting) {
+      onOffIntersection?.(entries)
+      return
+    }
+
+    onIntersection?.(entries)
+  }
+
+  const [observer] = useState(() =>
+    isClientSide
+      ? new IntersectionObserver(handleIntersect, options)
+      : undefined
+  )
+
+  useEffect(() => {
+    if (!ref.current || !observer) return
+
+    observer.observe(ref.current)
+
+    return () => {
+      observer.disconnect()
+    }
+  }, [ref, observer])
+
+  return { ref }
+}
+
+const isClientSide = typeof window !== "undefined"
