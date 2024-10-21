0eb47b6a4970f239eafcf30024b5200247aaad0a
Christopher Pappas <damassi.pappas@gmail.com>
2024-10-21 15:54:51 -0700

    fix(stackable-box): Fix css specificity on borders (#1404)

--- packages/palette/src/elements/StackableBorderBox/StackableBorderBox.tsx
+++ packages/palette/src/elements/StackableBorderBox/StackableBorderBox.tsx
@@ -7,11 +7,11 @@ export type StackableBorderBoxProps = BorderBoxProps
  * A stackable border box is a BorderBox that shares borders with its siblings.
  */
 export const StackableBorderBox = styled(BorderBox)<BorderBoxProps>`
-  :not(:first-child) {
+  &:not(:first-child) {
     border-top-left-radius: 0;
     border-top-right-radius: 0;
   }
-  :not(:last-child) {
+  &:not(:last-child) {
     border-bottom: 0;
     border-bottom-left-radius: 0;
     border-bottom-right-radius: 0;
