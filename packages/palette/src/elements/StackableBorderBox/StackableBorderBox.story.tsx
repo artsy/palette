3f46c3ed95df936c586989b53fed428f5cddb142
Christopher Pappas <damassi.pappas@gmail.com>
2024-10-21 15:50:34 -0700

    fix(stackable-box): fix css specificity

--- packages/palette/src/elements/StackableBorderBox/StackableBorderBox.story.tsx
+++ packages/palette/src/elements/StackableBorderBox/StackableBorderBox.story.tsx
@@ -1,5 +1,8 @@
 import React from "react"
 import { StackableBorderBox } from "./StackableBorderBox"
+import { Avatar } from "../Avatar/Avatar"
+import { Flex } from "../Flex"
+import { Button } from "../Button"
 
 export default {
   title: "Components/StackableBorderBox",
@@ -8,7 +11,23 @@ export default {
 export const Default = () => {
   return (
     <>
-      <StackableBorderBox>1</StackableBorderBox>
+      <StackableBorderBox>
+        <Flex>
+          <Flex>
+            <Avatar
+              size="xs"
+              src="https://picsum.photos/seed/example/110/110"
+            />
+          </Flex>
+          <Flex>
+            <Avatar
+              size="xs"
+              src="https://picsum.photos/seed/example/110/110"
+            />
+          </Flex>
+          <Button>Click me</Button>
+        </Flex>
+      </StackableBorderBox>
       <StackableBorderBox>2</StackableBorderBox>
       <StackableBorderBox>3</StackableBorderBox>
       <StackableBorderBox>4</StackableBorderBox>
