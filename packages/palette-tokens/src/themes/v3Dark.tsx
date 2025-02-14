ef419254b5ee0bb4b75182c31b1226cfbe65b732
Mounir Dhahri <soussouheros@gmail.com>
2025-02-14 15:49:43 +0100

    chore: update dark mode values
    * chore: add black90
    
    * chore: address review comments
    
    * chore: more colour changes

--- packages/palette-tokens/src/themes/v3Dark.tsx
+++ packages/palette-tokens/src/themes/v3Dark.tsx
@@ -14,46 +14,46 @@ const COLORS: Colors = {
   /** Suitable for text on black60 and darker */
   black5: "#1a1a1a",
   /** Suitable for text on black60 and darker */
-  white100: "#000000",
+  white100: "#121212",
 
   /** Suitable for text on black10 and lighter */
   blue200: "#d3d9fd",
   /** Suitable for text on black10 and lighter */
   blue150: "#a2b1fb",
   /** Suitable for text on black10 and lighter */
-  blue100: "#707eff",
+  blue100: "#0f77ff",
   /** Alias of blue100 */
   brand: "#707eff",
   /** Background only */
   blue15: "#474d8a",
   /** Background only */
-  blue10: "#31335e",
+  blue10: "#e6e7f5",
 
   /** Hover/down state and suitable for text on green10 */
   green150: "#c8fff0",
   /** Suitable for text on green10, black10 and lighter */
-  green100: "#98ffe2",
+  green100: "#019f73",
   /** Background only */
   green10: "#161d10",
 
   /** Hover/down state and suitable for text on yellow10 */
   yellow150: "#f0c65b",
   /** Suitable for text on black10 and lighter */
-  yellow100: "#d6ad1d",
+  yellow100: "#e2b929",
   /** Background only */
   yellow10: "#2b2203",
 
   /** Hover/down state and suitable for text on orange10 */
   orange150: "#e38b57",
   /** Suitable for text on black10 and lighter */
-  orange100: "#dd6a25",
+  orange100: "#da6722",
   /** Background only */
   orange10: "#2b1d12",
 
   /** Hover/down state and suitable for text on red10 */
   red150: "#f9d2d2",
   /** Suitable for text on red10, black10, and lighter */
-  red100: "#f68e98",
+  red100: "#e44738",
   /** Suitable for importance/urgency indicators */
   red50: "#d60012",
   /** Background only */
