ec4b02c83bdf02aa60e2d7e99cd4de64100d81ce
Rachel Quartararo <rquartararo@gmail.com>
2025-05-06 13:09:43 -0400

    upgrade to storybook 8

--- packages/palette/.storybook/main.js
+++ packages/palette/.storybook/main.js
@@ -1,16 +1,62 @@
-module.exports = {
-  stories: ["../src/**/*.story.(tsx|mdx)"],
+/** @type { import('@storybook/react-webpack5').StorybookConfig } */
+const config = {
+  stories: ["../src/**/*.story.@(tsx|mdx)"],
+
   addons: [
-    "@storybook/addon-actions",
     "@storybook/addon-links",
     "@storybook/addon-viewport",
+    "@storybook/addon-actions",
   ],
-  babel: async (options) => ({
-    ...options,
-    plugins: [["@babel/plugin-proposal-class-properties", { loose: true }]],
-  }),
-  core: {
-    builder: "webpack5",
+
+  framework: {
+    name: "@storybook/react-webpack5",
+    options: {},
   },
-  framework: "@storybook/react",
+
+  core: {
+    disableTelemetry: true,
+    builder: {
+      name: "webpack5",
+    },
+  },
+
+  typescript: {
+    check: false,
+    reactDocgen: "react-docgen-typescript",
+  },
+
+  babel: (config) => {
+    return { ...config, rootMode: "upward" }
+  },
+
+  webpackFinal: async (config) => {
+    config.module.rules.push({
+      test: /\.(ts|tsx|js|jsx)$/,
+      exclude: /node_modules/,
+      use: {
+        loader: "babel-loader",
+        options: {
+          presets: [
+            "@babel/preset-env",
+            "@babel/preset-typescript",
+            "@babel/preset-react",
+          ],
+          plugins: [
+            ["@babel/plugin-proposal-class-properties", { loose: true }],
+            [
+              "@babel/plugin-transform-private-property-in-object",
+              { loose: true },
+            ],
+            ["@babel/plugin-transform-private-methods", { loose: true }],
+          ],
+        },
+      },
+    })
+
+    config.resolve.extensions.push(".ts", ".tsx", ".js", ".jsx")
+
+    return config
+  },
 }
+
+export default config
