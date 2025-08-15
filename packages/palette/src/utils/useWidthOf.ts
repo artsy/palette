77b4f7a5f66b830befa62728393690cfb7b59f99
dzucconi <mail@damonzucconi.com>
2025-08-15 09:17:03 -0400

    fix(usewidthof): performance; ensures css changes propagate size change

--- packages/palette/src/utils/useWidthOf.ts
+++ packages/palette/src/utils/useWidthOf.ts
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react"
+import { useResizeObserver } from "./useResizeObserver"
 
 interface UseWidthOf {
   ref: React.MutableRefObject<HTMLElement | null>
@@ -8,22 +9,19 @@ interface UseWidthOf {
 export const useWidthOf = ({ ref }: UseWidthOf) => {
   const [width, setWidth] = useState(0)
 
+  // Set initial width on mount
   useEffect(() => {
     if (!ref.current) return
-
-    const handleResize = () => {
-      if (!ref.current) return
-      setWidth(ref.current.offsetWidth)
-    }
-
-    handleResize()
-
-    window.addEventListener("resize", handleResize)
-
-    return () => {
-      window.removeEventListener("resize", handleResize)
-    }
+    setWidth(ref.current.offsetWidth)
   }, [ref])
+
+  useResizeObserver<HTMLElement>({
+    target: (ref as unknown) as React.RefObject<HTMLElement>,
+    onResize: (entry) => {
+      const element = entry.target as HTMLElement
+      setWidth(element.offsetWidth)
+    },
+  })
 
   return { width }
 }
