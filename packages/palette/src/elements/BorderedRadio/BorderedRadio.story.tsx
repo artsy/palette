217d2f116098c1bf06224dc00f0d43b80c8b50c0
Erik <9088720+erikdstock@users.noreply.github.com>
2025-08-20 14:10:54 -0500

    demo: click on label triggers clickable inside it

--- packages/palette/src/elements/BorderedRadio/BorderedRadio.story.tsx
+++ packages/palette/src/elements/BorderedRadio/BorderedRadio.story.tsx
@@ -3,6 +3,7 @@ import React from "react"
 import { States } from "storybook-states"
 import { RadioGroup, RadioGroupProps } from "../RadioGroup"
 import { BorderedRadio } from "./BorderedRadio"
+import { Clickable } from "../Clickable"
 
 export default {
   title: "Components/BorderedRadio",
@@ -19,6 +20,34 @@ export const Default = () => {
             return (
               <BorderedRadio key={value} value={value}>
                 {value}
+              </BorderedRadio>
+            )
+          }
+        )}
+      </RadioGroup>
+    </States>
+  )
+}
+
+export const WithClickableInLabel = () => {
+  return (
+    <States<Partial<RadioGroupProps<string>>>
+      states={[{}, { defaultValue: "Aural" }, { deselectable: true }]}
+    >
+      <RadioGroup onSelect={fn()}>
+        {["Visual", "Linguistic", "Spatial", "Aural", "Gestural"].map(
+          (value) => {
+            return (
+              <BorderedRadio key={value} value={value}>
+                <>
+                  {value}{" "}
+                  <Clickable
+                    border="1px solid cyan"
+                    onClick={() => alert(`Clicked info for ${value}`)}
+                  >
+                    Trigger alert()
+                  </Clickable>
+                </>
               </BorderedRadio>
             )
           }
